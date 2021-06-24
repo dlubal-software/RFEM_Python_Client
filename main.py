c14af5046210ee2bd582eba9dd85f88e2548d734
Frank Faulstich <github@frank-faulstich.de>
2021-06-24 15:39:15 +0200

    Wrong type in argument for SurfaceLoad() corrected

--- main.py
+++ main.py
@@ -265,7 +265,7 @@ def main(hall_width_L, hall_height_h_o, hall_height_h_m, number_frames, frame_sp
 
 # -------------------------------------------------------------
     # Surface Loads
-    SurfaceLoad(1, 3, 3, 20000)
+    SurfaceLoad(1, 3, "3", 20000)
 
 # -------------------------------------------------------------
     # Finish client model
