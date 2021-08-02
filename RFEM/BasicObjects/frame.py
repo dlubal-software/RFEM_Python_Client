b4c562fe7cdb1ecd42359cd616d53b55afbeaa51
CarineKhalil22 <79253004+CarineKhalil22@users.noreply.github.com>
2021-08-02 13:26:22 +0200

    Update frame.py

--- RFEM/BasicObjects/frame.py
+++ RFEM/BasicObjects/frame.py
@@ -1,3 +1,31 @@
 from RFEM.initModel import *
 
-
+class Frame():
+    def __init__(self,
+                 no: int = 1,
+                 member_type = MemberType.TYPE_BEAM,
+                 start_node_no: int = 1,
+                 end_node_no: int = 2,
+                 rotation_angle: float = 0.0,
+                 start_section_no: int = 1,
+                 end_section_no: int = 1,
+                 start_member_hinge_no: int = 0,
+                 end_member_hinge_no: int = 0,
+                 member_type = MemberType.TYPE_#####,
+                 start_node_no: int = 1,
+                 end_node_no: int = 2,
+                 rotation_angle: float = 90.0,
+                 start_section_no: int = 1,
+                 end_section_no: int = 1,
+                 start_member_hinge_no: int = 0,
+                 end_member_hinge_no: int = 0,
+                 member_type = MemberType.TYPE_#####,
+                 start_node_no: int = 1,
+                 end_node_no: int = 2,
+                 rotation_angle: float = 90.0,
+                 start_section_no: int = 1,
+                 end_section_no: int = 1,
+                 start_member_hinge_no: int = 0,
+                 end_member_hinge_no: int = 0,
+                 comment: str = '',
+                 params: dict = {}):
