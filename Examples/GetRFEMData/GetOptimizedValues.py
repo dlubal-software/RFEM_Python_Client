e282dff66b24381768a09c24a067954766439cbc
Jaroslav Broz <jara.broz@gmail.com>
2021-08-03 11:56:01 +0200

    GetOptimizedValuesExample

---
+++ Examples/GetRFEMData/GetOptimizedValues.py
@@ -0,0 +1,48 @@
+import os
+import sys
+baseName = os.path.basename(__file__)
+dirName = os.path.dirname(__file__)
+print('basename:    ', baseName)
+print('dirname:     ', dirName)
+sys.path.append(dirName + r'/../..')
+
+from RFEM.Loads.surfaceLoad import *
+from RFEM.Loads.memberLoad import *
+from RFEM.Loads.nodalLoad import *
+from RFEM.LoadCasesAndCombinations.loadCase import *
+from RFEM.LoadCasesAndCombinations.staticAnalysisSettings import *
+from RFEM.TypesForMembers.memberHinge import *
+from RFEM.TypesForNodes.nodalSupport import *
+from RFEM.BasicObjects.solidSet import *
+from RFEM.BasicObjects.surfaceSet import *
+from RFEM.BasicObjects.memberSet import *
+from RFEM.BasicObjects.lineSet import *
+from RFEM.BasicObjects.opening import *
+from RFEM.BasicObjects.solid import *
+from RFEM.BasicObjects.surface import *
+from RFEM.BasicObjects.member import *
+from RFEM.BasicObjects.line import *
+from RFEM.BasicObjects.node import *
+from RFEM.BasicObjects.thickness import *
+from RFEM.BasicObjects.section import *
+from RFEM.BasicObjects.material import *
+from RFEM.initModel import *
+from RFEM.dataTypes import *
+from RFEM.enums import *
+
+
+if __name__ == "__main__":
+    
+    optimizationResults = clientModel.service.get_optimized_formula_parameters()
+   
+    if optimizationResults != None:
+        for i in range(0,len(optimizationResults.row)):
+            for j in  range (0, len(optimizationResults.row[i].section)):
+                
+                if hasattr(optimizationResults.row[i].section[j], 'top_text'):
+                    print(optimizationResults.row[i].section[j].top_text)
+                
+                for k in range(0,len(optimizationResults.row[i].section[j].elements[0])):
+                    print(optimizationResults.row[i].section[j].elements[0][k].column_name + " " + optimizationResults.row[i].section[j].elements[0][k].value)
+                
+    
