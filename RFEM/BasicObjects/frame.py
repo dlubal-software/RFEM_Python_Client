c00c6fb20c2945d615467e4c1bf28ae5b65ac3d3
Carine Khalil <79253004+CarineKhalil22@users.noreply.github.com>
2021-08-19 11:47:54 +0200

    Update frame.py

--- RFEM/BasicObjects/frame.py
+++ RFEM/BasicObjects/frame.py
@@ -6,17 +6,14 @@ class Frame():
                  no: int = 1,
                  start_node_no: int = 1,
                  end_node_no: int = 2,
-                 rotation_angle: float = 0.0,
                  start_section_no: int = 1,
                  end_section_no: int = 1,
                  start_node_no: int = 2,
                  end_node_no: int = 3,
-                 rotation_angle: float = 0.0,
                  start_section_no: int = 2,
                  end_section_no: int = 2,
                  start_node_no: int = 3,
                  end_node_no: int = 4,
-                 rotation_angle: float = 0.0,
                  start_section_no: int = 3,
                  end_section_no: int = 3,
                  comment: str = '',
@@ -28,11 +25,8 @@ class Frame():
         # Clears object atributes | Sets all atributes to None
         clearAtributes(clientObject)
 
-        # Member No.
+        # Frame No.
         clientObject.no = no
-
-        # Member Type
-        clientObject.type = member_type.name
 
         # Start Node No.
         clientObject.node_start = start_node_no
@@ -40,20 +34,11 @@ class Frame():
         # End Node No.
         clientObject.node_end = end_node_no
 
-        # Member Rotation Angle beta
-        clientObject.rotation_angle = rotation_angle
-
         # Start Section No.
         clientObject.section_start = start_section_no
 
         # End Section No.
         clientObject.section_end = end_section_no
-
-        # Start Member Hinge No.
-        clientObject.member_hinge_start = start_member_hinge_no
-
-        # End Member Hinge No.
-        clientObject.member_hinge_end = end_member_hinge_no
 
         # Comment
         clientObject.comment = comment
