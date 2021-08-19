164301fe604b6e24a68dd037eb63baed1348e14d
Carine Khalil <79253004+CarineKhalil22@users.noreply.github.com>
2021-08-19 09:27:22 +0200

    Update hall.py

--- Examples/Hall/hall.py
+++ Examples/Hall/hall.py
@@ -1,33 +1,36 @@
-Hall Example
-%import libraries*
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
 import sys
+baseName = os.path.basename(__file__)
+dirName = os.path.dirname(__file__)
+print('basename:    ', baseName)
+print('dirname:     ', dirName)
+sys.path.append(dirName + r'/../..')
 
-from RFEM.enums import *
-#from RFEM.window import *
-from RFEM.dataTypes import *
-from RFEM.initModel import *
-from RFEM.BasicObjects.material import *
-from RFEM.BasicObjects.section import *
-from RFEM.BasicObjects.thickness import *
-from RFEM.BasicObjects.node import *
-from RFEM.BasicObjects.line import *
-from RFEM.BasicObjects.member import *
-from RFEM.BasicObjects.surface import *
-from RFEM.BasicObjects.solid import *
-from RFEM.BasicObjects.opening import *
-from RFEM.BasicObjects.lineSet import *
-from RFEM.BasicObjects.memberSet import *
-from RFEM.BasicObjects.surfaceSet import *
-from RFEM.BasicObjects.solidSet import *
-from RFEM.TypesForNodes.nodalSupport import *
-from RFEM.TypesForMembers.memberHinge import *
-from RFEM.TypesForSurfaces.surfaceSupport import *
-from RFEM.LoadCasesAndCombinations.staticAnalysisSettings import *
-from RFEM.LoadCasesAndCombinations.loadCase import *
-from RFEM.Loads.nodalLoad import *
-from RFEM.Loads.memberLoad import *
-from RFEM.Loads.lineLoad import *
 from RFEM.Loads.surfaceLoad import *
+from RFEM.Loads.memberLoad import *
+from RFEM.Loads.nodalLoad import *
+from RFEM.LoadCasesAndCombinations.loadCase import *
+from RFEM.LoadCasesAndCombinations.staticAnalysisSettings import *
+from RFEM.TypesForMembers.memberHinge import *
+from RFEM.TypesForNodes.nodalSupport import *
+from RFEM.BasicObjects.solidSet import *
+from RFEM.BasicObjects.surfaceSet import *
+from RFEM.BasicObjects.memberSet import *
+from RFEM.BasicObjects.lineSet import *
+from RFEM.BasicObjects.opening import *
+from RFEM.BasicObjects.solid import *
+from RFEM.BasicObjects.surface import *
+from RFEM.BasicObjects.member import *
+from RFEM.BasicObjects.line import *
+from RFEM.BasicObjects.node import *
+from RFEM.BasicObjects.thickness import *
+from RFEM.BasicObjects.section import *
+from RFEM.BasicObjects.material import *
+from RFEM.initModel import *
+from RFEM.dataTypes import *
+from RFEM.enums import *
 
 if __name__ == '__main__':
 
