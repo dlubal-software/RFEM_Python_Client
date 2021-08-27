cd6d9a71950a0d62a6ddc0d4ffc6921e70359372
Carine Khalil <79253004+CarineKhalil22@users.noreply.github.com>
2021-08-27 18:08:33 +0200

    Update hall.py

--- Examples/Hall/hall.py
+++ Examples/Hall/hall.py
@@ -111,3 +111,126 @@ while True:
   print('Ready!')
 
   clientModel.service.finish_modification()
+  
+  
+  #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import sys
+import math
+baseName = os.path.basename(__file__)
+dirName = os.path.dirname(__file__)
+print('basename:    ', baseName)
+print('dirname:     ', dirName)
+sys.path.append(dirName + r'/../..')
+
+from RFEM.Loads.surfaceLoad import *
+from RFEM.Loads.memberLoad import *
+from RFEM.Loads.nodalLoad import *
+from RFEM.Loads.lineLoad import *
+from RFEM.LoadCasesAndCombinations.loadCase import *
+from RFEM.LoadCasesAndCombinations.staticAnalysisSettings import *
+from RFEM.TypesForMembers.memberHinge import *
+from RFEM.TypesForNodes.nodalSupport import *
+from RFEM.BasicObjects.solidSet import *
+from RFEM.BasicObjects.surfaceSet import *
+from RFEM.BasicObjects.memberSet import *
+from RFEM.BasicObjects.lineSet import *
+from RFEM.BasicObjects.opening import *
+from RFEM.BasicObjects.solid import *
+from RFEM.BasicObjects.surface import *
+from RFEM.BasicObjects.member import *
+from RFEM.BasicObjects.line import *
+from RFEM.BasicObjects.node import *
+from RFEM.BasicObjects.thickness import *
+from RFEM.BasicObjects.section import *
+from RFEM.BasicObjects.material import *
+from RFEM.initModel import *
+from RFEM.dataTypes import *
+from RFEM.enums import *
+
+# Import der Bibliotheken
+#from RFEM.window import *
+
+if __name__ == '__main__':
+    
+    
+  l = float(input('Length of the clear span in m: '))
+  n = float(input('Number of frames: '))
+  d = float(input('Distance between frames in m: '))
+  h = float(input('Height of frame in m: '))
+    
+  clientModel.service.begin_modification()
+
+  #nodes
+
+  i = 1
+  while i <= n:
+    j = (i-1) * 5
+    Node(j+1, 0.0, -(i-1)*d, 0.0)
+    Node(j+2, 0.0, -(i-1)*d, -h)
+    Node(j+3, l/2, -(i-1)*d, -h)
+    Node(j+4, l, -(i-1)*d, -h)
+    Node(j+5, l, -(i-1)*d, 0.0)
+    i += 1
+
+    
+  # Nodal Supports
+  i = 1
+  nodes_no = ""
+  while i <= n:
+    j = (i-1) * 5
+    nodes_no += str(j+1) + " "
+    nodes_no += str(j+5) + " "
+    i += 1
+    nodes_no = nodes_no.rstrip(nodes_no[-1])    
+    NodalSupport(1, nodes_no, NodalSupportType.HINGED, "Hinged support")
+
+  #members
+  Material (1 , 'S235')
+  Material (2, 'C25/30')
+ 
+    
+  Section (1, 'HEM 700',1)
+  Section (2, 'IPE 500',1)
+  
+  i = 1
+  while i <= n:
+    j = (i-1) * 5
+    k = (i-1) * 4
+    Member(k+1, MemberType.TYPE_BEAM, j+1, j+2, 0.0,  1, 1)
+    Member(k+2, MemberType.TYPE_BEAM, j+2, j+3, 0.0,  2, 2)
+    Member(k+3, MemberType.TYPE_BEAM, j+3, j+4, 0.0,  2, 2)
+    Member(k+4, MemberType.TYPE_BEAM, j+4, j+5, 0.0,  1, 1)
+    i += 1
+
+  # Vertical Bracing
+  def ask_user():
+    check = str(raw_input("Do you want to include bracing ? (Y/N): ")).lower().strip()
+    try:
+        if check[0] == 'y':
+            return 
+    
+              i = 1
+              while i <= n-1:
+                k = j + (i-1)*4
+               Member(k+1, MemberType.TYPE_TENSION, (i-1)*5+1, (i-1)*5+7 , 0.0,  2, 2)
+               Member(k+2, MemberType.TYPE_TENSION, (i-1)*5+2, (i-1)*5+6 , 0.0,  2, 2)
+               Member(k+3, MemberType.TYPE_TENSION, (i-1)*5+5, (i-1)*5+9 , 0.0,  2, 2)
+               Member(k+4, MemberType.TYPE_TENSION, (i-1)*5+4, (i-1)*5+10, 0.0,  2, 2)
+               i += 1
+        elif check[0] == 'n':
+            return False
+        else:
+            print('Invalid Input')
+            return ask_user()
+    except Exception as error:
+        print("Please enter valid inputs")
+        print(error)
+        return ask_user()
+
+
+ 
+
+  print('Ready!')
+  clientModel.service.finish_modification()
