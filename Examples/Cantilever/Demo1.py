77714e002e726e1afdb3af2f9183a8c614a9bcc5
CarineKhalil22 <79253004+CarineKhalil22@users.noreply.github.com>
2021-09-01 08:21:10 +0200

    changes

--- Examples/Cantilever/Demo1.py
+++ Examples/Cantilever/Demo1.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 import os
 import sys
+import math
 baseName = os.path.basename(__file__)
 dirName = os.path.dirname(__file__)
 print('basename:    ', baseName)
@@ -11,6 +12,7 @@ sys.path.append(dirName + r'/../..')
 from RFEM.Loads.surfaceLoad import *
 from RFEM.Loads.memberLoad import *
 from RFEM.Loads.nodalLoad import *
+from RFEM.Loads.lineLoad import *
 from RFEM.LoadCasesAndCombinations.loadCase import *
 from RFEM.LoadCasesAndCombinations.staticAnalysisSettings import *
 from RFEM.TypesForMembers.memberHinge import *
@@ -36,32 +38,98 @@ from RFEM.enums import *
 #from RFEM.window import *
 
 if __name__ == '__main__':
-    l = float(input('Length of the cantilever in m: '))
-    f = float(input('Force in kN: '))
+    
+    
+ l = float(input('Length of the clear span in m: '))
+ n = float(input('Number of frames: '))
+ d = float(input('Distance between frames in m: '))
+ h = float(input('Height of frame in m: '))
+ 
 
-    clientModel.service.begin_modification('new')
+    
+ clientModel.service.begin_modification()
 
-    Material(1, 'S235')
+ #nodes
 
-    Section(1, 'IPE 200')
+ i = 1
+ while i <= n:
+  j = (i-1) * 5
+  Node(j+1, 0.0, -(i-1)*d, 0.0)
+  Node(j+2, 0.0, -(i-1)*d, -h)
+  Node(j+3, l/2, -(i-1)*d, -h)
+  Node(j+4, l, -(i-1)*d, -h)
+  Node(j+5, l, -(i-1)*d, 0.0)
+  i += 1
 
-    Node(1, 0.0, 0.0, 0.0)
-    Node(2, l, 0.0, 0.0)
+    
+ # Nodal Supports
+ i = 1
+ nodes_no = ""
+ while i <= n:
+  j = (i-1) * 5
+  nodes_no += str(j+1) + " "
+  nodes_no += str(j+5) + " "
+  i += 1
+  nodes_no = nodes_no.rstrip(nodes_no[-1])    
+  NodalSupport(1, nodes_no, NodalSupportType.HINGED, "Hinged support")
 
-    Member(1, MemberType.TYPE_BEAM, 1, 2, 0.0, 1, 1)
 
-    NodalSupport(1, '1', NodalSupportType.FIXED)
+ #members
+ Material (1 , 'S235')
+ Material (2, 'C25/30')
+ 
+    
+ Section (1, 'HEM 700',1)
+ Section (2, 'IPE 500',1)
+ #members x direction 
+ i = 1
+ while i <= n:
+  j = (i-1) * 5
+  k = (i-1) * 4
+  Member(k+1, MemberType.TYPE_BEAM, j+1, j+2, 0.0,  1, 1)
+  Member(k+2, MemberType.TYPE_BEAM, j+2, j+3, 0.0,  2, 2)
+  Member(k+3, MemberType.TYPE_BEAM, j+3, j+4, 0.0,  2, 2)
+  Member(k+4, MemberType.TYPE_BEAM, j+4, j+5, 0.0,  1, 1)
+  i += 1
 
-    StaticAnalysisSettings(
-        1, '1. Ordnung', StaticAnalysisType.GEOMETRICALLY_LINEAR)
+ #members y direction 
+ i = 1
+ while i <= n-1:
+  j = (i-1) * 5
+  Member(int(4*n+i), MemberType.TYPE_BEAM, j+2, j+7, 0.0, 2, 2)
+  Member(int(4*n+i + n-1), MemberType.TYPE_BEAM, j+4, j+9, 0.0, 2, 2)
+  i += 1
+ 
+ #vertical bracing
 
-    LoadCase(1, 'Eigengewicht', AnalysisType.ANALYSIS_TYPE_STATIC,
-             1,  1, True, 0.0, 0.0, 1.0)
+ BracingV = input('Would you like to include vertical bracing?\n')
+ if BracingV.lower() == 'yes' or BracingV.lower() == 'y':
+  Material (3, 'EN AW-3004 H14')
+  Section (3, 'IPE 80',3)
+  i = 1
+  j = int(4*n + 3*(n-1))
+  while i <= n-1:
+   k = j + (i-1)*4
+   Member(k+1, MemberType.TYPE_TENSION, (i-1)*5+1, (i-1)*5+7, 0.0,  3, 3)
+   Member(k+2, MemberType.TYPE_TENSION, (i-1)*5+2, (i-1)*5+6, 0.0,  3, 3)
+   Member(k+3, MemberType.TYPE_TENSION, (i-1)*5+5, (i-1)*5+9, 0.0,  3, 3)
+   Member(k+4, MemberType.TYPE_TENSION, (i-1)*5+4, (i-1)*5+10, 0.0, 3, 3)
+   i += 1
 
-    NodalLoad(
-        1, 1, '2', LoadDirectionType.LOAD_DIRECTION_GLOBAL_Z_OR_USER_DEFINED_W, f*1000)
-
-    Calculate_all()
-    print('Ready!')
-
-    clientModel.service.finish_modification()
+ 
+ 
+ #horizontal bracing
+ BracingH = input('Would you like to include horizontal bracing?\n')
+ if BracingH.lower() == 'yes' or BracingH.lower() == 'y':
+  i = 1
+  while i <= n-1:
+   j = (i-1) * 5
+   Member(int(4*n+i), MemberType.TYPE_BEAM, j+2, j+8, 0.0, 3, 3)
+   Member(int(4*n+i + n-1), MemberType.TYPE_BEAM, j+3, j+7, 0.0, 3, 3)
+   Member(int(4*n+i + 2*n - 2), MemberType.TYPE_BEAM, j+3, j+9, 0.0, 3, 3)
+   Member(int(4*n+i + 3* n -3), MemberType.TYPE_BEAM, j+4, j+8, 0.0, 3, 3)
+   i += 1
+   
+ print("Preparing...")
+ print('Ready!')
+ clientModel.service.finish_modification()
