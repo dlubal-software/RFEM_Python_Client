1640960dc48fbd18ba717345099081188be43001
Carine Khalil <79253004+CarineKhalil22@users.noreply.github.com>
2021-09-02 14:47:05 +0200

    Update enums.py

--- RFEM/enums.py
+++ RFEM/enums.py
@@ -15,7 +15,20 @@ class MemberType(Enum):
     ...
     TYPE_BETWEEN_TWO_NODES, TYPE_BETWEEN_TWO_POINTS, TYPE_ON_LINE, TYPE_ON_MEMBER, TYPE_STANDARD = range (5)
     
+    
+  class NodeCoordinateSystemType(Enum):
+    '''
+    Node Coordinate System Type | Enum
+    '''
+    COORDINATE_SYSTEM_CARTESIAN, COORDINATE_SYSTEM_POLAR, COORDINATE_SYSTEM_X_CYLINDRICAL, COORDINATE_SYSTEM_Y_CYLINDRICAL, COORDINATE_SYSTEM_Z_CYLINDRICAL = range (5)
 
+
+  class NodeReferenceType(Enum):
+    '''
+    Node Reference Type| Enum
+    '''
+    REFERENCE_TYPE_L, REFERENCE_TYPE_XY, REFERENCE_TYPE_XZ, REFERENCE_TYPE_YZ = range (4)
+    
 
 class NodalSupportType(Enum):
     '''
