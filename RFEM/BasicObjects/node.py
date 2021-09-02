3890cfcc97da4ea463ce4f82a58582bdb8017d5d
Carine Khalil <79253004+CarineKhalil22@users.noreply.github.com>
2021-09-02 16:24:51 +0200

    Update node.py

--- RFEM/BasicObjects/node.py
+++ RFEM/BasicObjects/node.py
@@ -1,3 +1,6 @@
+from RFEM.enums import NodeType
+from RFEM.enums import NodeCoordinateSystemType
+from RFEM.enums import NodeReferenceType
 from RFEM.initModel import *
 
 class Node():
@@ -97,9 +100,17 @@ class Node():
 
     def BetweenTwoPoints(self,
                  no: int = 1,
-                 coordinate_X: float = 0.0,
-                 coordinate_Y: float = 0.0,
-                 coordinate_Z: float = 0.0,
+                 start_point_x: float = 0.0,
+                 start_point_y: float = 0.0,
+                 start_point_z: float = 0.0,
+                 end_point_x: float = 0.0,
+                 end_point_y:float = 0.0,
+                 end_point_z: float = 0.0,
+                 node_reference = NodeReferenceType.REFERENCE_TYPE_L,
+                 length_between_i_and_j: int = 1,
+                 parameters = [True, 0.5],
+                 offset_y: float = 0.0,
+                 offset_z: float = 0.0, 
                  comment: str = '',
                  params: dict = {}):
 
@@ -112,10 +123,40 @@ class Node():
         # Node No.
         clientObject.no = no
 
-        # Coordinates
-        clientObject.coordinate_1 = coordinate_X
-        clientObject.coordinate_2 = coordinate_Y
-        clientObject.coordinate_3 = coordinate_Z
+         
+        # Node Type
+        clientObject.type = NodeType.TYPE_BETWEEN_TWO_POINTS.name
+        
+
+        # Coordinates start point
+        clientObject.between_two_points_start_point_coordinate_1= start_point_x
+        clientObject.between_two_points_start_point_coordinate_2= start_point_y
+        clientObject.between_two_points_start_point_coordinate_3= start_point_z
+
+        # Coordinates end point
+
+        clientObject.between_two_points_end_point_coordinate_1= end_point_x
+        clientObject.between_two_points_end_point_coordinate_2= end_point_y
+        clientObject.between_two_points_end_point_coordinate_3= end_point_z
+         
+
+        # Length between i and j
+
+        clientObject.reference_type = node_reference.name
+
+        clientObject.reference_object_projected_length = length_between_i_and_j
+       
+       
+        # Distance between node k and start point
+
+        if parameters[0] == True:
+            clientObject.distance_from_start_relative = parameters[1]
+        elif parameters[0] == False:
+         clientObject.distance_from_start_absolute = parameters[1]
+
+        # offset local coordinates
+        clientObject.offset_in_local_direction_y= offset_y
+        clientObject.offset_in_local_direction_z= offset_z
 
         # Comment
         clientObject.comment = comment
