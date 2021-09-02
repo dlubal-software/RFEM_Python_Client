ee5c2bc3f7499d2e01d364c933d6657e643de4b6
Carine Khalil <79253004+CarineKhalil22@users.noreply.github.com>
2021-09-02 16:14:01 +0200

    Update node.py

--- RFEM/BasicObjects/node.py
+++ RFEM/BasicObjects/node.py
@@ -64,14 +64,25 @@ class Node():
         # Add Node to client model
         clientModel.service.set_node(clientObject)
 
-    def BetweenTwoNodes(self,
+     def BetweenTwoNodes(self,
                  no: int = 1,
                  start_node_no: int = 1,
                  end_node_no: int = 2,
-                 distance_start_node: int = 0,
-                 distance_end_node: int = 0,
+                 node_reference = NodeReferenceType.REFERENCE_TYPE_L,
+                 parameters = [True, 0.5],
+                 length_between_i_and_j: int = 1,
+                 offset_y: int = 0,
+                 offset_z: int = 0,
                  comment: str = '',
                  params: dict = {}):
+
+        '''
+        if distance_from_start_relative:
+            parameters = [True, %]
+        
+        if distance_from_start_absolute:
+            parameters[False, magnitude]
+        '''
 
         # Client model | Node
         clientObject = clientModel.factory.create('ns0:node')
@@ -82,26 +93,41 @@ class Node():
         # Node No.
         clientObject.no = no
         
+        # Node Type
+        clientObject.type = NodeType.TYPE_BETWEEN_TWO_NODES.name
+        
         # Start Node No.
-        clientObject.node_start = start_node_no
+        clientObject.between_two_nodes_start_node = start_node_no
 
         # End Node No.
-        clientObject.node_end = end_node_no
+        clientObject.between_two_nodes_end_node = end_node_no
         
-        # Distance Start Node.
-        clientObject = distance_start_node
-        
-        # Distance Start Node. 
-        clientObject = distance_end_node
+        # Length between i and j
+        clientObject.reference_type = node_reference.name
+
+        if parameters[0] == True:
+            clientObject.distance_from_start_relative = parameters[1]
+        elif parameters[0] == False:
+         clientObject.distance_from_start_absolute = parameters[1]
+
+        clientObject.reference_object_projected_length = length_between_i_and_j
+
+        # Offset_local_y
+        clientObject.offset_in_local_direction_y = offset_y
+
+        # Offset_local_z
+        clientObject.offset_in_local_direction_z = offset_z
 
         # Comment
         clientObject.comment = comment
+
 
         for key in params:
             clientObject[key] = params[key]
 
         # Add Node to client model
         clientModel.service.set_node(clientObject)
+
 
     def BetweenTwoPoints(self,
                  no: int = 1,
