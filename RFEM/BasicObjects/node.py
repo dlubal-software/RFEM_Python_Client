983cd6a14e8b67468cc70307bdfd7d0f8ee7cc51
Carine Khalil <79253004+CarineKhalil22@users.noreply.github.com>
2021-09-02 16:38:27 +0200

    Update node.py

--- RFEM/BasicObjects/node.py
+++ RFEM/BasicObjects/node.py
@@ -1,3 +1,6 @@
+from RFEM.enums import NodeType
+from RFEM.enums import NodeCoordinateSystemType
+from RFEM.enums import NodeReferenceType
 from RFEM.initModel import *
 
 class Node():
@@ -127,10 +130,10 @@ class Node():
         clientModel.service.set_node(clientObject)
 
     def OnLine(self,
-                 no: int = 1,
-                 coordinate_X: float = 0.0,
-                 coordinate_Y: float = 0.0,
-                 coordinate_Z: float = 0.0,
+                 reference_line: int = 1,
+                 node_reference = NodeReferenceType.REFERENCE_TYPE_L,
+                 length_between_i_and_j: int = 1,
+                 parameters = [True, 0.5],
                  comment: str = '',
                  params: dict = {}):
 
@@ -141,12 +144,24 @@ class Node():
         clearAtributes(clientObject)
 
         # Node No.
-        clientObject.no = no
+        clientObject.on_line_reference_line = reference_line
 
-        # Coordinates
-        clientObject.coordinate_1 = coordinate_X
-        clientObject.coordinate_2 = coordinate_Y
-        clientObject.coordinate_3 = coordinate_Z
+        # Node Type
+        clientObject.type = NodeType.TYPE_ON_LINE.name
+
+        # Length between i and j
+
+        clientObject.reference_type = node_reference.name
+
+        clientObject.reference_object_projected_length = length_between_i_and_j
+       
+       
+        # Distance between node k and start point
+
+        if parameters[0] == True:
+            clientObject.distance_from_start_relative = parameters[1]
+        elif parameters[0] == False:
+         clientObject.distance_from_start_absolute = parameters[1] 
 
         # Comment
         clientObject.comment = comment
