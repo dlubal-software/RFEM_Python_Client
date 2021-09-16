76d13a9e375294f651c97596508c77d3af2b7767
Björn Steinhagen <bjoern.steinhagen@dlubal.com>
2021-09-16 13:24:21 +0200

    Rectangular Load Second Commit
    All functions defined and preliminary tests conducted. All works. More tests to be conducted.
    Docstrings in abeyance.

--- RFEM/Loads/freeLoad.py
+++ RFEM/Loads/freeLoad.py
@@ -1,17 +1,18 @@
 from RFEM.initModel import *
 from RFEM.enums import *
 from enum import Enum
+from math import *
 
 class FreeLoad():
 
     def ConcentratedLoad(self,
                  no: int = 1,
                  load_case_no: int = 1,
+                 surfaces_no = '1',
                  load_direction = FreeConcentratedLoadLoadDirection.LOAD_DIRECTION_GLOBAL_Z,
                  load_projection = FreeLoadLoadProjection.LOAD_PROJECTION_XY_OR_UV,
                  load_type = FreeConcentratedLoadLoadType.LOAD_TYPE_FORCE,
                  load_parameter = [1000, 0, 0],
-                 surfaces_no = '1',
                  comment: str = '',
                  params: dict = {}):
                  
@@ -70,11 +71,11 @@ class FreeLoad():
     def LineLoad(self,
                  no: int = 1,
                  load_case_no: int = 1,
+                 surfaces_no = '1',
                  load_direction = FreeLineLoadLoadDirection.LOAD_DIRECTION_LOCAL_Z,
                  load_distribution = FreeLineLoadLoadDistribution.LOAD_DISTRIBUTION_UNIFORM,
                  load_projection = FreeLoadLoadProjection.LOAD_PROJECTION_XY_OR_UV,
                  load_parameter = [],
-                 surfaces_no = '1',
                  comment: str = '',
                  params: dict = {}):
                  
@@ -142,11 +143,13 @@ class FreeLoad():
     def RectangularLoad(self,
                  no: int = 1,
                  load_case_no: int = 1,
-                 load_direction = FreeLineLoadLoadDirection.LOAD_DIRECTION_LOCAL_Z,
-                 load_distribution = FreeLineLoadLoadDistribution.LOAD_DISTRIBUTION_UNIFORM,
-                 load_projection = FreeLoadLoadProjection.LOAD_PROJECTION_XY_OR_UV,
-                 load_parameter = [],
                  surfaces_no = '1',
+                 load_distribution = FreeRectangularLoadLoadDistribution.LOAD_DISTRIBUTION_UNIFORM,
+                 load_projection = FreeLoadLoadProjection.LOAD_PROJECTION_XY_OR_UV,
+                 load_direction = FreeRectangularLoadLoadDirection.LOAD_DIRECTION_GLOBAL_Z_TRUE,
+                 load_magnitude_parameter = [],
+                 load_location = FreeRectangularLoadLoadLocationRectangle.LOAD_LOCATION_RECTANGLE_CORNER_POINTS,
+                 load_location_parameter = [],
                  comment: str = '',
                  params: dict = {}):
                  
@@ -178,24 +181,191 @@ class FreeLoad():
         # Load Direction
         clientObject.load_direction = load_direction.name
         
-        # Load Parameter
-        if load_distribution.name == 'LOAD_DISTRIBUTION_UNIFORM':
-            if len(load_parameter) != 5:
-                raise Exception('WARNING: The load parameter needs to be of length 5. Kindly check list inputs for completeness and correctness.')
-            clientObject.magnitude_uniform = load_parameter[0]
-            clientObject.load_location_first_x = load_parameter[1]
-            clientObject.load_location_first_y = load_parameter[2]
-            clientObject.load_location_second_x = load_parameter[3]
-            clientObject.load_location_second_y = load_parameter[4]
-        elif load_distribution.name == 'LOAD_DISTRIBUTION_LINEAR':
-            if len(load_parameter) != 6:
-                raise Exception('WARNING: The load parameter needs to be of length 6. Kindly check list inputs for completeness and correctness.')
-            clientObject.magnitude_first = load_parameter[0]
-            clientObject.magnitude_second = load_parameter[1]
-            clientObject.load_location_first_x = load_parameter[2]
-            clientObject.load_location_first_y = load_parameter[3]
-            clientObject.load_location_second_x = load_parameter[4]
-            clientObject.load_location_second_y = load_parameter[5]
+        # Load Magnitude Parameter
+        if load_distribution.name == 'LOAD_DISTRIBUTION_UNIFORM' or load_distribution.name == 'LOAD_DISTRIBUTION_VARYING_IN_Z' or load_distribution.name == 'LOAD_DISTRIBUTION_VARYING_ALONG_PERIMETER' or load_distribution.name == 'LOAD_DISTRIBUTION_VARYING_IN_Z_AND_ALONG_PERIMETER':
+            if len(load_magnitude_parameter) != 1:
+                raise Exception('WARNING: The load parameter for the selected distribution needs to be of length 1. Kindly check list inputs for completeness and correctness.')
+            clientObject.magnitude_uniform = load_magnitude_parameter[0]
+        elif load_distribution.name == 'LOAD_DISTRIBUTION_LINEAR_FIRST' or load_distribution.name == 'LOAD_DISTRIBUTION_LINEAR_SECOND':
+            if len(load_magnitude_parameter) != 2:
+                raise Exception('WARNING: The load parameter for the selected distribution needs to be of length 2. Kindly check list inputs for completeness and correctness.')
+            clientObject.magnitude_linear_first = load_magnitude_parameter[0]
+            clientObject.magnitude_linear_second = load_magnitude_parameter[1]
+
+        # Load Location Parameter
+        clientObject.load_location_rectangle = load_location.name
+
+        if load_location.name == 'LOAD_LOCATION_RECTANGLE_CORNER_POINTS':
+
+            if load_distribution.name == 'LOAD_DISTRIBUTION_UNIFORM' or load_distribution.name == 'LOAD_DISTRIBUTION_LINEAR_FIRST' or load_distribution.name == 'LOAD_DISTRIBUTION_LINEAR_SECOND':
+                if len(load_location_parameter) != 5:
+                    raise Exception('WARNING: The load location parameter for the designated location and distribution type needs to be of length 5. Kindly check list inputs for completeness and correctness.')
+                clientObject.load_location_first_x = load_location_parameter[0]
+                clientObject.load_location_first_y = load_location_parameter[1]
+                clientObject.load_location_second_x = load_location_parameter[2]
+                clientObject.load_location_second_y = load_location_parameter[3]
+                clientObject.axis_start_angle = load_location_parameter[4] * (pi/180)
+
+            elif load_distribution.name == 'LOAD_DISTRIBUTION_VARYING_IN_Z':
+                if len(load_location_parameter) != 5:
+                    raise Exception('WARNING: The load location parameter for the designated location and distribution type needs to be of length 5. Kindly check list inputs for completeness and correctness.')
+                clientObject.load_location_first_x = load_location_parameter[0]
+                clientObject.load_location_first_y = load_location_parameter[1]
+                clientObject.load_location_second_x = load_location_parameter[2]
+                clientObject.load_location_second_y = load_location_parameter[3]
+
+                clientObject.load_varying_in_z_parameters = clientModel.factory.create('ns0:free_rectangular_load.load_varying_in_z_parameters')
+                varying_in_z = load_location_parameter[4]
+                for i in range(len(varying_in_z)):
+                    frllvp = clientModel.factory.create('ns0:free_rectangular_load_load_varying_in_z_parameters')
+                    frllvp.no = i+1
+                    frllvp.distance = varying_in_z[i][0]
+                    frllvp.factor = varying_in_z[i][1]
+                    clientObject.load_varying_in_z_parameters.free_rectangular_load_load_varying_in_z_parameters.append(frllvp)
+
+            elif load_distribution.name == 'LOAD_DISTRIBUTION_VARYING_ALONG_PERIMETER':
+                if len(load_location_parameter) != 8:
+                    raise Exception('WARNING: The load location parameter for the designated location and distribution type needs to be of length 9. Kindly check list inputs for completeness and correctness.')
+                clientObject.load_location_first_x = load_location_parameter[0]
+                clientObject.load_location_first_y = load_location_parameter[1]
+                clientObject.load_location_second_x = load_location_parameter[2]
+                clientObject.load_location_second_y = load_location_parameter[3]
+                clientObject.axis_definition_p1_x = load_location_parameter[4][0]
+                clientObject.axis_definition_p1_y = load_location_parameter[4][1]
+                clientObject.axis_definition_p1_z = load_location_parameter[4][2]
+                clientObject.axis_definition_p2_x = load_location_parameter[5][0]
+                clientObject.axis_definition_p2_y = load_location_parameter[5][1]
+                clientObject.axis_definition_p2_z = load_location_parameter[5][2]
+                clientObject.axis_start_angle = load_location_parameter[6]
+
+                clientObject.load_varying_along_perimeter_parameters = clientModel.factory.create('ns0:free_rectangular_load.load_varying_along_perimeter_parameters')
+                varying_along_perimeter = load_location_parameter[7]
+                for i in range(len(varying_along_perimeter)):
+                    frllvapp = clientModel.factory.create('ns0:free_rectangular_load_load_varying_along_perimeter_parameters')
+                    frllvapp.no = i+1
+                    frllvapp.alpha = varying_along_perimeter[i][0] * (pi/180)    
+                    frllvapp.factor = varying_along_perimeter[i][1]
+                    clientObject.load_varying_along_perimeter_parameters.free_rectangular_load_load_varying_along_perimeter_parameters.append(frllvapp)
+
+            elif load_distribution.name == 'LOAD_DISTRIBUTION_VARYING_IN_Z_AND_ALONG_PERIMETER':
+                if len(load_location_parameter) != 9:
+                    raise Exception('WARNING: The load location parameter for the designated location and distribution type needs to be of length 9. Kindly check list inputs for completeness and correctness.')
+                clientObject.load_location_first_x = load_location_parameter[0]
+                clientObject.load_location_first_y = load_location_parameter[1]
+                clientObject.load_location_second_x = load_location_parameter[2]
+                clientObject.load_location_second_y = load_location_parameter[3]
+
+                clientObject.load_varying_in_z_parameters = clientModel.factory.create('ns0:free_rectangular_load.load_varying_in_z_parameters')
+                varying_in_z = load_location_parameter[4]
+                for i in range(len(varying_in_z)):
+                    frllvp = clientModel.factory.create('ns0:free_rectangular_load_load_varying_in_z_parameters')
+                    frllvp.no = i+1
+                    frllvp.distance = varying_in_z[i][0]
+                    frllvp.factor = varying_in_z[i][1]
+                    clientObject.load_varying_in_z_parameters.free_rectangular_load_load_varying_in_z_parameters.append(frllvp)
+
+                clientObject.axis_definition_p1_x = load_location_parameter[5][0]
+                clientObject.axis_definition_p1_y = load_location_parameter[5][1]
+                clientObject.axis_definition_p1_z = load_location_parameter[5][2]
+                clientObject.axis_definition_p2_x = load_location_parameter[5][0]
+                clientObject.axis_definition_p2_y = load_location_parameter[6][1]
+                clientObject.axis_definition_p2_z = load_location_parameter[6][2]
+                clientObject.axis_start_angle = load_location_parameter[7]
+
+                clientObject.load_varying_along_perimeter_parameters = clientModel.factory.create('ns0:free_rectangular_load.load_varying_along_perimeter_parameters')
+                varying_along_perimeter = load_location_parameter[8]
+                for i in range(len(varying_along_perimeter)):
+                    frllvapp = clientModel.factory.create('ns0:free_rectangular_load_load_varying_along_perimeter_parameters')
+                    frllvapp.no = i+1
+                    frllvapp.alpha = varying_along_perimeter[i][0] * (pi/180)
+                    frllvapp.factor = varying_along_perimeter[i][1]
+                    clientObject.load_varying_along_perimeter_parameters.free_rectangular_load_load_varying_along_perimeter_parameters.append(frllvapp)
+
+        elif load_location.name == 'LOAD_LOCATION_RECTANGLE_CENTER_AND_SIDES':
+    
+            if load_distribution.name == 'LOAD_DISTRIBUTION_UNIFORM' or load_distribution.name == 'LOAD_DISTRIBUTION_LINEAR_FIRST' or load_distribution.name == 'LOAD_DISTRIBUTION_LINEAR_SECOND':
+                if len(load_location_parameter) != 5:
+                    raise Exception('WARNING: The load location parameter for the designated location and distribution type needs to be of length 5. Kindly check list inputs for completeness and correctness.')
+                clientObject.load_location_center_x = load_location_parameter[0]
+                clientObject.load_location_center_y = load_location_parameter[1]
+                clientObject.load_location_center_side_a = load_location_parameter[2]
+                clientObject.load_location_center_side_b = load_location_parameter[3]
+                clientObject.axis_start_angle = load_location_parameter[4] * (pi/180)
+
+            elif load_distribution.name == 'LOAD_DISTRIBUTION_VARYING_IN_Z':
+                if len(load_location_parameter) != 5:
+                    raise Exception('WARNING: The load location parameter for the designated location and distribution type needs to be of length 5. Kindly check list inputs for completeness and correctness.')
+                clientObject.load_location_center_x = load_location_parameter[0]
+                clientObject.load_location_center_y = load_location_parameter[1]
+                clientObject.load_location_center_side_a = load_location_parameter[2]
+                clientObject.load_location_center_side_b = load_location_parameter[3]
+
+                clientObject.load_varying_in_z_parameters = clientModel.factory.create('ns0:free_rectangular_load.load_varying_in_z_parameters')
+                varying_in_z = load_location_parameter[4]
+                for i in range(len(varying_in_z)):
+                    frllvp = clientModel.factory.create('ns0:free_rectangular_load_load_varying_in_z_parameters')
+                    frllvp.no = i+1
+                    frllvp.distance = varying_in_z[i][0]
+                    frllvp.factor = varying_in_z[i][1]
+                    clientObject.load_varying_in_z_parameters.free_rectangular_load_load_varying_in_z_parameters.append(frllvp)
+
+            elif load_distribution.name == 'LOAD_DISTRIBUTION_VARYING_ALONG_PERIMETER':
+                if len(load_location_parameter) != 8:
+                    raise Exception('WARNING: The load location parameter for the designated location and distribution type needs to be of length 9. Kindly check list inputs for completeness and correctness.')
+                clientObject.load_location_center_x = load_location_parameter[0]
+                clientObject.load_location_center_y = load_location_parameter[1]
+                clientObject.load_location_center_side_a = load_location_parameter[2]
+                clientObject.load_location_center_side_b = load_location_parameter[3]
+                clientObject.axis_definition_p1_x = load_location_parameter[4][0]
+                clientObject.axis_definition_p1_y = load_location_parameter[4][1]
+                clientObject.axis_definition_p1_z = load_location_parameter[4][2]
+                clientObject.axis_definition_p2_x = load_location_parameter[5][0]
+                clientObject.axis_definition_p2_y = load_location_parameter[5][1]
+                clientObject.axis_definition_p2_z = load_location_parameter[5][2]
+                clientObject.axis_start_angle = load_location_parameter[6]
+
+                clientObject.load_varying_along_perimeter_parameters = clientModel.factory.create('ns0:free_rectangular_load.load_varying_along_perimeter_parameters')
+                varying_along_perimeter = load_location_parameter[7]
+                for i in range(len(varying_along_perimeter)):
+                    frllvapp = clientModel.factory.create('ns0:free_rectangular_load_load_varying_along_perimeter_parameters')
+                    frllvapp.no = i+1
+                    frllvapp.alpha = varying_along_perimeter[i][0] * (pi/180)    
+                    frllvapp.factor = varying_along_perimeter[i][1]
+                    clientObject.load_varying_along_perimeter_parameters.free_rectangular_load_load_varying_along_perimeter_parameters.append(frllvapp)
+
+            elif load_distribution.name == 'LOAD_DISTRIBUTION_VARYING_IN_Z_AND_ALONG_PERIMETER':
+                if len(load_location_parameter) != 9:
+                    raise Exception('WARNING: The load location parameter for the designated location and distribution type needs to be of length 9. Kindly check list inputs for completeness and correctness.')
+                clientObject.load_location_center_x = load_location_parameter[0]
+                clientObject.load_location_center_y = load_location_parameter[1]
+                clientObject.load_location_center_side_a = load_location_parameter[2]
+                clientObject.load_location_center_side_b = load_location_parameter[3]
+
+                clientObject.load_varying_in_z_parameters = clientModel.factory.create('ns0:free_rectangular_load.load_varying_in_z_parameters')
+                varying_in_z = load_location_parameter[4]
+                for i in range(len(varying_in_z)):
+                    frllvp = clientModel.factory.create('ns0:free_rectangular_load_load_varying_in_z_parameters')
+                    frllvp.no = i+1
+                    frllvp.distance = varying_in_z[i][0]
+                    frllvp.factor = varying_in_z[i][1]
+                    clientObject.load_varying_in_z_parameters.free_rectangular_load_load_varying_in_z_parameters.append(frllvp)
+
+                clientObject.axis_definition_p1_x = load_location_parameter[5][0]
+                clientObject.axis_definition_p1_y = load_location_parameter[5][1]
+                clientObject.axis_definition_p1_z = load_location_parameter[5][2]
+                clientObject.axis_definition_p2_x = load_location_parameter[5][0]
+                clientObject.axis_definition_p2_y = load_location_parameter[6][1]
+                clientObject.axis_definition_p2_z = load_location_parameter[6][2]
+                clientObject.axis_start_angle = load_location_parameter[7]
+
+                clientObject.load_varying_along_perimeter_parameters = clientModel.factory.create('ns0:free_rectangular_load.load_varying_along_perimeter_parameters')
+                varying_along_perimeter = load_location_parameter[8]
+                for i in range(len(varying_along_perimeter)):
+                    frllvapp = clientModel.factory.create('ns0:free_rectangular_load_load_varying_along_perimeter_parameters')
+                    frllvapp.no = i+1
+                    frllvapp.alpha = varying_along_perimeter[i][0] * (pi/180)
+                    frllvapp.factor = varying_along_perimeter[i][1]
+                    clientObject.load_varying_along_perimeter_parameters.free_rectangular_load_load_varying_along_perimeter_parameters.append(frllvapp)
 
         # Comment
         clientObject.comment = comment
