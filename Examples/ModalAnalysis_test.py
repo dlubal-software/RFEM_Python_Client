e0c9c3f18e629e8d39749861a9b8907c6c5cd174
KaratasD <dogukan.karatas@dlubal.com>
2021-09-27 10:59:38 +0200

    tested

--- Examples/ModalAnalysis_test.py
+++ Examples/ModalAnalysis_test.py
@@ -1,5 +1,6 @@
 import sys
 sys.path.append(".")
+from RFEM.LoadCasesAndCombinations.modalanalysisSettings import ModalAnalysisSettings
 from RFEM.Loads.surfaceLoad import *
 from RFEM.Loads.memberLoad import *
 from RFEM.Loads.nodalLoad import *
@@ -28,8 +29,36 @@ if __name__ == '__main__':
 
     clientModel.service.begin_modification()
 
-    # Create Material 
+    # Create Material
     Material(1, 'S235')
+
+    # Crate Section
+    Section(1, 'IPE 300', 1)
+
+    # Create Nodes
+    Node(1, 0, 0, 0)
+    Node(2, 0, 0, -5)
+
+    # Create Member
+    Member(1, MemberType.TYPE_BEAM, 1, 2, 0, 1, 1)
+
+    # Create Nodal Support
+    NodalSupport(1, '1', NodalSupportType.FIXED) 
+
+    # Static Analysis Settings
+    StaticAnalysisSettings(1, 'Geometrically Linear', StaticAnalysisType.GEOMETRICALLY_LINEAR)
+
+    # Modal Analysis Settings
+    ModalAnalysisSettings(2, 'Modal Analysis Settings', ModalSolutionMethod.METHOD_LANCZOS, ModalMassConversionType.MASS_CONVERSION_TYPE_Z_COMPONENTS_OF_LOADS, 
+    ModalMassMatrixType.MASS_MATRIX_TYPE_DIAGONAL, 2, [False, False, False, False, True, True])
+
+    # Load Case Static
+    LoadCase(1, 'DEAD', AnalysisType.ANALYSIS_TYPE_STATIC, 1, 1, True, 0, 0, 1)
+
+    # Load Case Modal
+    LoadCase(2, 'MODAL', AnalysisType.ANALYSIS_TYPE_MODAL, 2, 1, False)
+
+    Calculate_all()
 
     print('Ready!')
 
--- RFEM/LoadCasesAndCombinations/modalanalysisSettings.py
+++ RFEM/LoadCasesAndCombinations/modalanalysisSettings.py
@@ -8,14 +8,14 @@ class ModalAnalysisSettings():
                  solution_method = ModalSolutionMethod.METHOD_LANCZOS,
                  mass_conversion_type = ModalMassConversionType.MASS_CONVERSION_TYPE_Z_COMPONENTS_OF_LOADS,
                  mass_matrix_type = ModalMassMatrixType.MASS_MATRIX_TYPE_CONSISTENT,
-                 number_of_modes_method = ModalModeNumberMethod.NUMBER_OF_MODES_METHOD_USER_DEFINED,
                  number_of_modes : int = 4,
                  acting_masses = [],
+                 neglect_masses = ModalNeglectMasses.E_NEGLECT_MASSES_NO_NEGLECTION,
                  comment: str = '',
                  params: dict = {}):
 
         # Client model | Surface
-        clientObject = clientModel.factory.create('ns0:static_analysis_settings')
+        clientObject = clientModel.factory.create('ns0:modal_analysis_settings')
 
         # Clears object atributes | Sets all atributes to None
         clearAtributes(clientObject)
@@ -31,7 +31,25 @@ class ModalAnalysisSettings():
         clientObject.solution_method = solution_method.name
 
         # Mass Conversion Type
-        
+        clientObject.mass_conversion_type = mass_conversion_type.name
+
+        # Mass Matrix Type
+        clientObject.mass_matrix_type = mass_matrix_type.name
+
+        # Number of Modes
+        clientObject.number_of_modes_method = "NUMBER_OF_MODES_METHOD_USER_DEFINED"
+        clientObject.number_of_modes = number_of_modes
+
+        # Acting Masses
+        clientObject.acting_masses_about_axis_x_enabled = acting_masses[0]
+        clientObject.acting_masses_about_axis_y_enabled = acting_masses[1]
+        clientObject.acting_masses_about_axis_z_enabled = acting_masses[2]
+        clientObject.acting_masses_in_direction_x_enabled = acting_masses[3]
+        clientObject.acting_masses_in_direction_y_enabled = acting_masses[4]
+        clientObject.acting_masses_in_direction_z_enabled = acting_masses[5]
+
+        # Neglect Masses
+        clientObject.neglect_masses = neglect_masses.name
 
         # Comment
         clientObject.comment = comment
@@ -41,4 +59,4 @@ class ModalAnalysisSettings():
             clientObject[key] = params[key]
 
         # Add Static Analysis Settings to client model
-        clientModel.service.set_static_analysis_settings(clientObject)
+        clientModel.service.set_modal_analysis_settings(clientObject)
