ddf2a92c641e7592cda5a4d32f80ad58ac8e43f2
Björn Steinhagen <bjoern.steinhagen@dlubal.com>
2021-09-27 14:18:29 +0200

    First Commit
    - Design situation types for DIN added. SOAP requires numeric code. The relevant name associated with the numeric code is provide in the DocStrings;
    - Ability for manual naming and 'active' fields; and
    - Test file added.

--- RFEM/LoadCasesAndCombinations/designSituation.py
+++ RFEM/LoadCasesAndCombinations/designSituation.py
@@ -4,8 +4,31 @@ from RFEM.enums import SetType
 class DesignSituation():
     def __init__(self,
                  no: int = 1,
+                 user_defined_name: bool = False,
+                 name = None,
+                 active: bool = True,
+                 design_situation_type: int = 6122,
                  comment: str = '',
                  params: dict = {}):
+        
+        '''
+        design_situation_type:
+            6122 : ULS (EQU) - Permanent and transient,
+            6993 : ULS (EQU) - Accidental - psi-1,1,
+            6994 : ULS (EQU) - Accidental - psi-2,1,
+            6995 : ULS (EQU) - Accidental - Snow - psi-1,1,
+            6996 : ULS (EQU) - Accidental - Snow - psi-2,1,
+            6997 : ULS (EQU) - Seismic,
+            7007 : ULS (STR/GEO) - Permanent and transient - Eq. 6.10,
+            7010 : ULS (STR/GEO) - Accidental - psi-1,1,
+            7011 : ULS (STR/GEO) - Accidental - psi-2,1,
+            7012 : ULS (STR/GEO) - Accidental - Snow - psi-1,1,
+            7013 : ULS (STR/GEO) - Accidental - Snow - psi-2,1,
+            7014 : ULS (STR/GEO) - Seismic,
+            6193 : SLS - Characteristic,
+            6194 : SLS - Frequent,
+            6195 : SLS - Quasi-permanent
+        '''
 
         # Client model | Design Situation
         clientObject = clientModel.factory.create('ns0:design_situation')
@@ -15,6 +38,22 @@ class DesignSituation():
 
         # Design Situation No.
         clientObject.no = no
+
+        # Design Situation Name
+        clientObject.user_defined_name_enabled = user_defined_name
+        if user_defined_name == True:
+            if name == None:
+                raise Exception('WARNING: A user defined design situation name was requested. As such, the name parameter cannot be empty.')
+            clientObject.name = name
+
+        # Design Situation Active
+        clientObject.active = active
+        
+        # Design Situation Type
+        clientObject.design_situation_type = design_situation_type
+
+        # Design Situation Comment
+        clientObject.comment = comment
 
         # Adding optional parameters via dictionary
         for key in params:
