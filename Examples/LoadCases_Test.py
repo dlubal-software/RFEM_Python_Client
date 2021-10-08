951b2cd5db88a9efaa9805ec846a0023ff790f22
Björn Steinhagen <bjoern.steinhagen@dlubal.com>
2021-10-08 08:58:14 +0200

    Updated Test File

--- Examples/LoadCases_Test.py
+++ Examples/LoadCases_Test.py
@@ -32,10 +32,10 @@ if __name__ == '__main__':
 	clientModel.service.begin_modification('new')
 
 	StaticAnalysisSettings()
-	LoadCase(1, 'SW', True, AnalysisType.ANALYSIS_TYPE_STATIC, 1, DIN_Action_Category['1A'], [True, 0, 0, 1])
-	LoadCase(2, 'SDL', True, AnalysisType.ANALYSIS_TYPE_STATIC, 1, DIN_Action_Category['1C'], [True, 0.1, 0.1, 0])
-	LoadCase(3, 'Snow', True, AnalysisType.ANALYSIS_TYPE_STATIC, 1, DIN_Action_Category['4A'], [False])
-	LoadCase(4, 'Wind', False, AnalysisType.ANALYSIS_TYPE_STATIC, 1, DIN_Action_Category['5'], [False])
+	LoadCase.StaticAnalysis(LoadCase, 1, 'SW', True, 1, DIN_Action_Category['1A'], [True, 0, 0, 1])
+	LoadCase.StaticAnalysis(LoadCase, 2, 'SDL', True,  1, DIN_Action_Category['1C'], [True, 0.1, 0.1, 0])
+	LoadCase.StaticAnalysis(LoadCase, 3, 'Snow', True,  1, DIN_Action_Category['4A'], [False])
+	LoadCase.StaticAnalysis(LoadCase, 4, 'Wind', False,  1, DIN_Action_Category['5'], [False])
 
 
 	clientModel.service.finish_modification()
