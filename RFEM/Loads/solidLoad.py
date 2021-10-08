5e1100d429126617cc9ce3095c5e0cf8049b78f9
Frank Faulstich <github@frank-faulstich.de>
2021-10-08 11:53:56 +0200

    Code formatting

--- RFEM/Loads/solidLoad.py
+++ RFEM/Loads/solidLoad.py
@@ -100,6 +100,7 @@ class SolidLoad():
         # Add Solid Load to client model
         clientModel.service.set_solid_load(load_case_no, clientObject)
 
+
     def Temperature(self,
                     no: int = 1,
                     load_case_no: int = 1,
@@ -150,6 +151,7 @@ class SolidLoad():
 
         # Add Solid Load to client model
         clientModel.service.set_solid_load(load_case_no, clientObject)
+
 
     def Strain(self,
                no: int = 1,
@@ -212,11 +214,68 @@ class SolidLoad():
         # Add Solid Load to client model
         clientModel.service.set_solid_load(load_case_no, clientObject)
 
-    def Motion():
-        pass
+
+    def Motion(self,
+               no: int = 1,
+               load_case_no: int = 1,
+               solids_no: str= '1',
+               angular_velocity: float = 0,
+               angular_acceleration: float = 0,
+               axis_definition_p1_x: float = 0,
+               axis_definition_p1_y: float = 0,
+               axis_definition_p1_z: float = 0,
+               axis_definition_p2_x: float = 0,
+               axis_definition_p2_y: float = 0,
+               axis_definition_p2_z: float = 0,
+               comment: str = '',
+               params: dict = {}):
+        
+        # Client model | Solid Load
+        clientObject = clientModel.factory.create('ns0:solid_load')
+
+        # Clears object attributes | Sets all attributes to None
+        clearAtributes(clientObject)
+
+        # Load No.
+        clientObject.no = no
+        
+        # Load Case No.
+        clientObject.load_case = load_case_no
+        
+        # Assigned Solid No.
+        clientObject.solids = ConvertToDlString(solids_no)
+
+        # Load Type
+        clientObject.load_type = SolidLoadType.LOAD_TYPE_ROTARY_MOTION.name
+
+        # Velocity
+        clientObject.angular_velocity = angular_velocity
+
+        # Acceleration
+        clientObject.angular_acceleration = angular_acceleration
+
+        # Axis Definition
+        clientObject.axis_definition_p1_x = axis_definition_p1_x
+        clientObject.axis_definition_p1_y = axis_definition_p1_y
+        clientObject.axis_definition_p1_z = axis_definition_p1_z
+        clientObject.axis_definition_p2_x = axis_definition_p2_x
+        clientObject.axis_definition_p2_y = axis_definition_p2_y
+        clientObject.axis_definition_p2_z = axis_definition_p2_z
+
+        # Comment
+        clientObject.comment = comment
+        
+        # Adding optional parameters via dictionary
+        for key in params:
+            clientObject[key] = params[key]
+        
+        # Add Solid Load to client model
+        clientModel.service.set_solid_load(load_case_no, clientObject)
+
 
     def Buoyancy():
         pass
+
 
     def Gass():
         pass
--- testSolidLoads.py
+++ testSolidLoads.py
@@ -1,4 +1,3 @@
-#from RFEM.Loads.solidLoad import SolidLoad
 from RFEM.Loads.solidLoad import SolidLoad
 from RFEM.enums import *
 from RFEM.window import *
@@ -30,6 +29,7 @@ if __name__ == '__main__':
 
     Material(1, 'S235')
     
+    # Solid 1
     Node(1, 0.0, 0.0, 0.0)
     Node(2, 10.0, 0.0, 0.0)
     Node(3, 10.0, 10.0, 0.0)
@@ -90,7 +90,68 @@ if __name__ == '__main__':
     SolidLoad.Strain(SolidLoad, 10, 1, '1', SolidLoadDistribution.LOAD_DISTRIBUTION_LINEAR_IN_Y, 0.01, 0.02, 0.03, 5, 0.04, 0.05, 0.06, 8)
     SolidLoad.Strain(SolidLoad, 11, 1, '1', SolidLoadDistribution.LOAD_DISTRIBUTION_LINEAR_IN_Z, 0.01, 0.02, 0.03, 5, 0.04, 0.05, 0.06, 1)
 
+    SolidLoad.Motion(SolidLoad, 12, 1, '1', 5, 2, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0)
 
+    # Solid 2
+    Node(9, 0.0, 20.0, 0.0)
+    Node(10, 10.0, 20.0, 0.0)
+    Node(11, 10.0, 30.0, 0.0)
+    Node(12, 0.0, 30.0, 0.0)
+
+    Node(13, 0.0, 20.0, -5.0)
+    Node(14, 10.0, 20.0, -5.0)
+    Node(15, 10.0, 30.0, -5.0)
+    Node(16, 0.0, 30.0, -5.0)
+
+    Line(13, '9 10')
+    Line(14, '10 11')
+    Line(15, '11 12')
+    Line(16, '12 9')
+
+    Line(17, '13 14')
+    Line(18, '14 15')
+    Line(19, '15 16')
+    Line(20, '16 13')
+
+    Line(21, '9 13')
+    Line(22, '10 14')
+    Line(23, '11 15')
+    Line(24, '12 16')
+
+    Surface(7, '13-16', 1)
+    Surface(8, '17-20', 1)
+    Surface(9, '13 22 17 21', 1)
+    Surface(10, '15 23 19 24', 1)
+    Surface(11, '16 21 20 24', 1)
+    Surface(12, '14 22 18 23', 1)
+
+    Solid(2, '7-12', 1)
+
+    # Solid 3
+    Node(17, 20.0, 20.0, 0.0)
+    Node(18, 20.0, 30.0, 0.0)
+    Node(19, 20.0, 20.0, -5.0)
+    Node(20, 20.0, 30.0, -5.0)
+
+    Line(25, '10 17')
+    Line(26, '17 18')
+    Line(27, '18 11')
+    Line(28, '14 19')
+    Line(29, '19 20')
+    Line(30, '20 15')
+    Line(31, '17 19')
+    Line(32, '18 20')
+
+    Surface(13, '25 26 27 14', 1)
+    Surface(14, '28 29 30 18', 1)
+    Surface(15, '25 31 28 22', 1)
+    Surface(16, '27 32 30 23', 1)
+    Surface(17, '26 31 29 32', 1)
+
+    Solid(3, '13-17,12', 1)
+
+    # Solid Set
+    SolidSet.ContinuousSolids(SolidSet, 1, '2 3')
 
     #Calculate_all()
     print('Ready!')
