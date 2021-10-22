2ae988bf9a23db0da12288b317c29c86954e0ceb
Björn Steinhagen <bjoern.steinhagen@dlubal.com>
2021-10-22 14:02:35 +0200

    Functoin to init Model

--- RFEM/firstFreeNumber.py
+++
@@ -1,7 +0,0 @@
-from RFEM.initModel import *
-from RFEM.enums import ObjectTypes
-
-def FirstFreeNumber(type = ObjectTypes.E_OBJECT_TYPE_MEMBER,
-                    parent_no: int = 0):
-
-                    return clientModel.service.get_first_free_number(type.name, parent_no)
--- RFEM/initModel.py
+++ RFEM/initModel.py
@@ -298,3 +298,8 @@ def ParseCSVResultsFromSelectedFileToDict(filePath: str):
 def ParseXMLResultsFromSelectedFileToDict(filePath: str):
     
     return __parseXMLAsDictionary(filePath)
+
+def FirstFreeNumber(type = ObjectTypes.E_OBJECT_TYPE_MEMBER,
+            parent_no: int = 0):
+
+            return clientModel.service.get_first_free_number(type.name, parent_no)
