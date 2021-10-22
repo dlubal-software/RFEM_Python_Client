a802ce0451e82428aac10010ef5c9d7bf08c3ae4
Frank Faulstich <github@frank-faulstich.de>
2021-10-22 13:20:43 +0200

    Use load_parameter in Strain()

--- RFEM/Loads/solidLoad.py
+++ RFEM/Loads/solidLoad.py
@@ -109,7 +109,15 @@ class SolidLoad():
                     load_parameter = None,
                     comment: str = '',
                     params: dict = {}):
-        
+        '''
+        load_parameter:
+            LOAD_DISTRIBUTION_UNIFORM: load_parameter = magnitude
+            LOAD_DISTRIBUTION_LINEAR_IN_X: load_parameter = [magnitude_1, magnitude_2, node_1, node_2]
+            LOAD_DISTRIBUTION_LINEAR_IN_Y: load_parameter = [magnitude_1, magnitude_2, node_1, node_2]
+            LOAD_DISTRIBUTION_LINEAR_IN_Z: load_parameter = [magnitude_1, magnitude_2, node_1, node_2]
+        params:
+            {''}
+        '''
         # Client model | Solid Load
         clientObject = clientModel.factory.create('ns0:solid_load')
 
@@ -155,17 +163,18 @@ class SolidLoad():
                load_case_no: int = 1,
                solids_no: str= '1',
                load_distribution = SolidLoadDistribution.LOAD_DISTRIBUTION_UNIFORM,
-               strain_magnitude_x1: float = 0,
-               strain_magnitude_y1: float = 0,
-               strain_magnitude_z1: float = 0,
-               node_1: int = 1,
-               strain_magnitude_x2: float = 0,
-               strain_magnitude_y2: float = 0,
-               strain_magnitude_z2: float = 0,
-               node_2: int = 2,
+               load_parameter = None,
                comment: str = '',
                params: dict = {}):
-
+        '''
+        load_parameter:
+            LOAD_DISTRIBUTION_UNIFORM: load_parameter = [strain_uniform_magnitude_x, strain_uniform_magnitude_y, strain_uniform_magnitude_z]
+            LOAD_DISTRIBUTION_LINEAR_IN_X: load_parameter = [strain_magnitude_x1, strain_magnitude_y1, strain_magnitude_z1, strain_magnitude_x2, strain_magnitude_y2, strain_magnitude_z2, node_1, node_2]
+            LOAD_DISTRIBUTION_LINEAR_IN_Y: load_parameter = [strain_magnitude_x1, strain_magnitude_y1, strain_magnitude_z1, strain_magnitude_x2, strain_magnitude_y2, strain_magnitude_z2, node_1, node_2]
+            LOAD_DISTRIBUTION_LINEAR_IN_Z: load_parameter = [strain_magnitude_x1, strain_magnitude_y1, strain_magnitude_z1, strain_magnitude_x2, strain_magnitude_y2, strain_magnitude_z2, node_1, node_2]
+        params:
+            {''}
+        '''
         # Client model | Solid Load
         clientObject = clientModel.factory.create('ns0:solid_load')
 
@@ -186,18 +195,18 @@ class SolidLoad():
 
         # Load Distribution
         if load_distribution.name == "LOAD_DISTRIBUTION_UNIFORM":
-            clientObject.strain_uniform_magnitude_x = strain_magnitude_x1
-            clientObject.strain_uniform_magnitude_y = strain_magnitude_y1
-            clientObject.strain_uniform_magnitude_z = strain_magnitude_z1
+            clientObject.strain_uniform_magnitude_x = load_parameter[1]
+            clientObject.strain_uniform_magnitude_y = load_parameter[2]
+            clientObject.strain_uniform_magnitude_z = load_parameter[3]
         else:
-            clientObject.strain_magnitude_x1 = strain_magnitude_x1
-            clientObject.strain_magnitude_y1 = strain_magnitude_y1
-            clientObject.strain_magnitude_z1 = strain_magnitude_z1
-            clientObject.strain_magnitude_x2 = strain_magnitude_x2
-            clientObject.strain_magnitude_y2 = strain_magnitude_y2
-            clientObject.strain_magnitude_z2 = strain_magnitude_z2
-            clientObject.node_1 = node_1
-            clientObject.node_2 = node_2
+            clientObject.strain_magnitude_x1 = load_parameter[1]
+            clientObject.strain_magnitude_y1 = load_parameter[2]
+            clientObject.strain_magnitude_z1 = load_parameter[3]
+            clientObject.strain_magnitude_x2 = load_parameter[4]
+            clientObject.strain_magnitude_y2 = load_parameter[5]
+            clientObject.strain_magnitude_z2 = load_parameter[6]
+            clientObject.node_1 = load_parameter[7]
+            clientObject.node_2 = load_parameter[8]
         
         clientObject.load_distribution = load_distribution.name
 
