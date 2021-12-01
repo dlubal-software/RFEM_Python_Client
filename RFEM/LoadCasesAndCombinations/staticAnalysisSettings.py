a1ff18020a378f57bf3989178e5748f8274baae2
Jaroslav Broz <jara.broz@gmail.com>
2021-12-01 13:44:51 +0100

    Fix of Model in staticAnalysisSetting object

--- RFEM/LoadCasesAndCombinations/staticAnalysisSettings.py
+++ RFEM/LoadCasesAndCombinations/staticAnalysisSettings.py
@@ -43,7 +43,7 @@ class StaticAnalysisSettings():
             clientObject[key] = params[key]
 
         # Add Static Analysis Settings to client model
-        clientModel.service.set_static_analysis_settings(clientObject)
+        Model.clientModel.service.set_static_analysis_settings(clientObject)
 
     def GeometricallyLinear(self,
                   no: int = 1,
@@ -72,7 +72,7 @@ class StaticAnalysisSettings():
         """
 
         # Client model
-        clientObject = clientModel.factory.create('ns0:static_analysis_settings')
+        clientObject = Model.clientModel.factory.create('ns0:static_analysis_settings')
 
         # Clears object atributes | Sets all atributes to None
         clearAtributes(clientObject)
@@ -149,7 +149,7 @@ class StaticAnalysisSettings():
             clientObject[key] = params[key]
 
         # Add Static Analysis Settings to client model
-        clientModel.service.set_static_analysis_settings(clientObject)
+        Model.clientModel.service.set_static_analysis_settings(clientObject)
 
     def LargeDeformation(self,
                   no: int = 1,
@@ -192,7 +192,7 @@ class StaticAnalysisSettings():
         """
 
         # Client model
-        clientObject = clientModel.factory.create('ns0:static_analysis_settings')
+        Model.clientObject = Model.clientModel.factory.create('ns0:static_analysis_settings')
 
         # Clears object atributes | Sets all atributes to None
         clearAtributes(clientObject)
@@ -316,7 +316,7 @@ class StaticAnalysisSettings():
             clientObject[key] = params[key]
 
         # Add Static Analysis Settings to client model
-        clientModel.service.set_static_analysis_settings(clientObject)
+        Model.clientModel.service.set_static_analysis_settings(clientObject)
 
     def SecondOrderPDelta (self,
                   no: int = 1,
@@ -353,7 +353,7 @@ class StaticAnalysisSettings():
         """
 
         # Client model
-        clientObject = clientModel.factory.create('ns0:static_analysis_settings')
+        Model.clientObject = clientModel.factory.create('ns0:static_analysis_settings')
 
         # Clears object atributes | Sets all atributes to None
         clearAtributes(clientObject)
