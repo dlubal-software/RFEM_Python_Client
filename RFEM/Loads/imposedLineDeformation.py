9c56b04462a4e969692b9143089eaf6c45f90e4c
Frank Faulstich <github@frank-faulstich.de>
2022-01-03 15:53:03 +0100

    Parameter Definition

--- RFEM/Loads/imposedLineDeformation.py
+++ RFEM/Loads/imposedLineDeformation.py
@@ -3,5 +3,25 @@ from RFEM.enums import *
 
 class ImposedLineDeformation():
 
-    def __init__(self) -> None:
+    def __init__(self,
+                 no: int = 1,
+                 load_case_no: int = 1,
+                 line_no: str = '',
+                 load_parameter = None,
+                 comment: str = '',
+                 params: dict = {}):
+
+        '''
+        load_parameter:
+            load_parameter = [imposed_displacement_line_start_x, 
+                              imposed_displacement_line_start_y, 
+                              imposed_displacement_line_start_z, 
+                              imposed_rotation_line_start, 
+                              imposed_displacement_line_end_x, 
+                              imposed_displacement_line_end_y, 
+                              imposed_displacement_line_end_z, 
+                              imposed_rotation_line_end]
+        '''
+
+
         pass
