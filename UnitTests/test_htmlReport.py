042222c8876aa2a80701fba70125c5874daa9763
MichalO <ondrej.michal@dlubal.cz>
2022-03-08 09:28:46 +0100

    unit test fix

--- UnitTests/test_htmlReport.py
+++ UnitTests/test_htmlReport.py
@@ -7,14 +7,16 @@ PROJECT_ROOT = os.path.abspath(os.path.join(
 sys.path.append(PROJECT_ROOT)
 from RFEM.Reports.html import ExportResultTablesToHtml
 from RFEM.initModel import Model
+from os import path, getcwd
 
 if Model.clientModel is None:
     Model()
 
 def test_html_report():
     Model.clientModel.service.delete_all()
-    # Model.clientModel.service.run_script('..\\scripts\\internal\\Demos\\Demo-006 Castellated Beam.js')
-    Model.clientModel.service.run_script('..\\scripts\\internal\\Demos\\Demo-001 Hall.js')
+    Model.clientModel.service.run_script('..\\scripts\\internal\\Demos\\Demo-003 Castellated Beam.js')
     Model.clientModel.service.calculate_all(False)
 
-    ExportResultTablesToHtml('d:\\Sources\\results')
+    dirname = path.join(getcwd(), path.dirname(__file__))
+    print(dirname)
+    ExportResultTablesToHtml(path.join(dirname, 'testResults'))
