8c33015e725fff418f60e3d12f9ac4e343c639de
KaratasD <dogukan.karatas@dlubal.com>
2022-03-28 15:37:33 +0200

    deleted unnecessary changes

--- Examples/Cantilever/Demo1.py
+++ Examples/Cantilever/Demo1.py
@@ -9,7 +9,7 @@ print('dirname:     ', dirName)
 sys.path.append(dirName + r'/../..')
 
 from RFEM.enums import *
-from RFEM.initModel import Model, Calculate_all, SetAddonStatus, AddOn
+from RFEM.initModel import Model, Calculate_all
 from RFEM.BasicObjects.material import Material
 from RFEM.BasicObjects.section import Section
 from RFEM.BasicObjects.node import Node
@@ -19,7 +19,6 @@ from RFEM.LoadCasesAndCombinations.staticAnalysisSettings import StaticAnalysisS
 from RFEM.LoadCasesAndCombinations.loadCase import LoadCase
 from RFEM.Loads.nodalLoad import NodalLoad
 from RFEM.Calculate.meshSettings import GetModelInfo
-from RFEM.TypesForSteelDesign.steelMemberRotationalRestraints import SteelMemberRotationalRestraint
 
 if __name__ == '__main__':
     l = float(input('Length of the cantilever in m: '))
@@ -28,11 +27,6 @@ if __name__ == '__main__':
     Model(True, "Demo1") # crete new model called Demo1
     Model.clientModel.service.begin_modification()
 
-    SetAddonStatus(Model.clientModel, AddOn.steel_design_active, True)
-
-    SteelMemberRotationalRestraint(1, [False], SteelMemberRotationalRestraintType.TYPE_CONTINUOUS, "1", "",
-        categories=["Grade S275", "Arval (-) 35/207 - 0.63 (b: 1) | DIN 18807 | Arval", SteelMemberRotationalRestraintPositionofSheeting.SHEETING_POSITION_NEGATIVE, SteelMemberRotationalRestraintContinuousBeamEffect.CONTINUOUS_BEAM_EFFECT_END_PANEL, True],
-        parameters = [205000000000.0, 0.00063, 7.5e-08, 0.207, 0.106, 5200.0, 3.0])
 
     Material(1, 'S235')
 
@@ -54,21 +48,21 @@ if __name__ == '__main__':
     NodalLoad(1, 1, '2', LoadDirectionType.LOAD_DIRECTION_GLOBAL_Z_OR_USER_DEFINED_W, f*1000)
     Model.clientModel.service.finish_modification()
 
-    #Calculate_all()
+    Calculate_all()
 
-    # # model status
-    # modelStatus = GetModelInfo()
-    # print("Model is calculated" if modelStatus.property_has_results else "Model is not calculated")
-    # print("Model contains printout report" if modelStatus.property_has_printout_report else "Model has not printout report")
-    # print ("Model contains " +  str(modelStatus.property_node_count) + " nodes")
-    # print ("Model contains " +  str(modelStatus.property_line_count) + " lines")
-    # print ("Model contains " +  str(modelStatus.property_member_count) + " members")
-    # print ("Model contains " +  str(modelStatus.property_surface_count) + " surfaces")
-    # print ("Model contains " +  str(modelStatus.property_solid_count) + " solids")
-    # print ("Model contains " +  str(modelStatus.property_lc_count) + " load cases")
-    # print ("Model contains " +  str(modelStatus.property_co_count) + " load combinations")
-    # print ("Model contains " +  str(modelStatus.property_rc_count) + " result classes")
-    # print ("Model weight " +   str(modelStatus.property_weight))
-    # print ("Model dimension x " + str(modelStatus.property_dimensions.x))
-    # print ("Model dimension y " + str(modelStatus.property_dimensions.y))
-    # print ("Model dimension z " + str(modelStatus.property_dimensions.z))
+    # model status
+    modelStatus = GetModelInfo()
+    print("Model is calculated" if modelStatus.property_has_results else "Model is not calculated")
+    print("Model contains printout report" if modelStatus.property_has_printout_report else "Model has not printout report")
+    print ("Model contains " +  str(modelStatus.property_node_count) + " nodes")
+    print ("Model contains " +  str(modelStatus.property_line_count) + " lines")
+    print ("Model contains " +  str(modelStatus.property_member_count) + " members")
+    print ("Model contains " +  str(modelStatus.property_surface_count) + " surfaces")
+    print ("Model contains " +  str(modelStatus.property_solid_count) + " solids")
+    print ("Model contains " +  str(modelStatus.property_lc_count) + " load cases")
+    print ("Model contains " +  str(modelStatus.property_co_count) + " load combinations")
+    print ("Model contains " +  str(modelStatus.property_rc_count) + " result classes")
+    print ("Model weight " +   str(modelStatus.property_weight))
+    print ("Model dimension x " + str(modelStatus.property_dimensions.x))
+    print ("Model dimension y " + str(modelStatus.property_dimensions.y))
+    print ("Model dimension z " + str(modelStatus.property_dimensions.z))
