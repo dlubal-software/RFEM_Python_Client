9b66f268dfd5d9f5cce58ecdd20ea222b17dc07e
FrankFaulstich <36745927+FrankFaulstich@users.noreply.github.com>
2022-04-05 09:09:37 +0200

    init finished

--- RFEM/Imperfections/memberImpferfection.py
+++ RFEM/Imperfections/memberImpferfection.py
@@ -15,6 +15,8 @@ class MemberImperfection():
         '''
         Args:
             no (int): Member Imperfection Tag
+            imperfection_case_no (int): number of imperfection case for this member imperfection
+            members (str) : List of member numbers
             comment (str, optional): Comments
             params (dict, optional): Parameters
         '''
@@ -41,9 +43,44 @@ class MemberImperfection():
         Model.clientModel.service.set_member_imperfection(imperfection_case_no, clientObject)
 
 
-    def InitalSway(self,
-                   ):
-        pass
+    def InitialSwayRelative(self,
+                   no: int = 1,
+                   imperfection_case_no: int = 1,
+                   members: str ='1',
+                   sway: float =300.0, # Das muss in clientObject eingefügt werden.
+                   direction: , # Da muss eine Aufzählung definiert werden.
+                   comment: str = '',
+                   params: dict = {}):
+        '''
+        Args:
+            no (int): Member Imperfection Tag
+            imperfection_case_no (int): number of imperfection case for this member imperfection
+            members (str) : List of member numbers
+            sway (float):
+            comment (str, optional): Comments
+            params (dict, optional): Parameters
+        '''
+        # Hier geht es weiter!
+        # Client model | Imperfection Case
+        clientObject = Model.clientModel.factory.create('ns0:member_imperfection')
+
+        # Clears object atributes | Sets all atributes to None
+        clearAtributes(clientObject)
+
+        # Member Imperfection No.
+        clientObject.no = no
+
+        clientObject.members = ConvertToDlString(members)
+
+        # Comment
+        clientObject.comment = comment
+
+        # Adding optional parameters via dictionary
+        for key in params:
+            clientObject[key] = params[key]
+
+        # Add Member Imperfection to client model
+        Model.clientModel.service.set_member_imperfection(imperfection_case_no, clientObject)
 
     def InitalBow(self,
                    ):
