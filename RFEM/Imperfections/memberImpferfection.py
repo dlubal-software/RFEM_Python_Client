d02d6cc4930bc81e2036e7d80dab2808cf5e0e23
FrankFaulstich <36745927+FrankFaulstich@users.noreply.github.com>
2022-04-08 14:03:58 +0200

    InitialBowRelativeAndCriterion() finished

--- RFEM/Imperfections/memberImpferfection.py
+++ RFEM/Imperfections/memberImpferfection.py
@@ -123,7 +123,7 @@ class MemberImperfection():
 
         clientObject.members = ConvertToDlString(members)
 
-        # Sway
+        # Bow
 
         clientObject.basic_value_relative = bow
 
@@ -145,5 +145,58 @@ class MemberImperfection():
         Model.clientModel.service.set_member_imperfection(imperfection_case_no, clientObject)
 
     def InitialBowRelativeAndCriterion(self,
-                   ):
-        pass
+                                       no: int = 1,
+                                       imperfection_case_no: int = 1,
+                                       members: str = '1',
+                                       active_criterion = ImperfectionActivationCriterion.ACTIVITY_CRITERION_ALWAYS,
+                                       bow = [250.0],
+                                       imperfection_direction = ImperfectionDirection.IMPERFECTION_DIRECTION_LOCAL_Y,
+                                       comment: str = '',
+                                       params: dict = {}):
+        '''
+        Args:
+            no (int): Member Imperfection Tag
+            imperfection_case_no (int): number of imperfection case for this member imperfection
+            members (str) : List of member numbers
+            bow (float):
+            active_criterion ():
+            imperfection_direction
+            comment (str, optional): Comments
+            params (dict, optional): Additional parameters
+        '''
+        # Client model | Imperfection Case
+        clientObject = Model.clientModel.factory.create('ns0:member_imperfection')
+
+        # Clears object atributes | Sets all atributes to None
+        clearAtributes(clientObject)
+
+        # Member Imperfection No.
+        clientObject.no = no
+
+        clientObject.members = ConvertToDlString(members)
+
+        # Criterion
+        clientObject.active_criterion = active_criterion.name
+
+        if active_criterion == ImperfectionActivationCriterion.ACTIVITY_CRITERION_DEFINE:
+            clientObject.active_bow = bow[1]
+
+        # Bow
+        clientObject.basic_value_relative = bow[0]
+
+        # Imperfection Direction
+
+        clientObject.imperfection_direction = imperfection_direction.name
+
+        clientObject.imperfection_type = 'IMPERFECTION_TYPE_INITIAL_BOW_AND_CRITERION'
+        clientObject.definition_type = 'DEFINITION_TYPE_RELATIVE'
+
+        # Comment
+        clientObject.comment = comment
+
+        # Adding optional parameters via dictionary
+        for key in params:
+            clientObject[key] = params[key]
+
+        # Add Member Imperfection to client model
+        Model.clientModel.service.set_member_imperfection(imperfection_case_no, clientObject)
--- test.py
+++ test.py
@@ -52,7 +52,11 @@ if __name__ == '__main__':
     StaticAnalysisSettings(1, 'LINEAR', StaticAnalysisType.GEOMETRICALLY_LINEAR)
     StaticAnalysisSettings(2, 'NONLINEAR', StaticAnalysisType.SECOND_ORDER_P_DELTA)
 
-    DesignSituation(1, True,'My DS', True)
+    #DesignSituation(1, True,'My DS', True)
+    DsDict = {
+        "design_situation_type": ""
+    }
+    DesignSituation(1,DesignSituationType.DESIGN_SITUATION_TYPE_A_ACCIDENTAL, True, 'Test', 'My Comment', DsDict)
 
     LoadCase(1, 'Test 1')
     LoadCase(2, 'Test 2', [False])
@@ -60,6 +64,8 @@ if __name__ == '__main__':
     LoadCombination(1, AnalysisType.ANALYSIS_TYPE_STATIC, 1, [False], 2, False, False, False, True, [[1.1, 1, 0, False], [1.5, 2, 0, False]], 'My Combination')
     LoadCombination(2, AnalysisType.ANALYSIS_TYPE_STATIC, 1, [False], 2, False, False, False, True, [[1.1, 1, 0, False], [1.5, 2, 0, False]], 'My Combination')
     LoadCombination(3, AnalysisType.ANALYSIS_TYPE_STATIC, 1, [False], 2, False, False, False, True, [[1.1, 1, 0, False], [1.5, 2, 0, False]], 'My Combination')
+    LoadCombination(4, AnalysisType.ANALYSIS_TYPE_STATIC, 1, [False], 2, False, False, False, True, [[1.1, 1, 0, False], [1.5, 2, 0, False]], 'My Combination')
+    LoadCombination(5, AnalysisType.ANALYSIS_TYPE_STATIC, 1, [False], 2, False, False, False, True, [[1.1, 1, 0, False], [1.5, 2, 0, False]], 'My Combination')
 
     NodalLoad(1, 1, '2', LoadDirectionType.LOAD_DIRECTION_GLOBAL_Z_OR_USER_DEFINED_W, 10000)
     NodalLoad(1, 2, '2', LoadDirectionType.LOAD_DIRECTION_GLOBAL_Z_OR_USER_DEFINED_W, 5000)
@@ -67,9 +73,12 @@ if __name__ == '__main__':
     ImperfectionCase.Local(ImperfectionCase, 1, '1', '1-2', 'My Comment' )
     ImperfectionCase.Local(ImperfectionCase, 2, '2')
     ImperfectionCase.Local(ImperfectionCase, 3, '3')
+    ImperfectionCase.Local(ImperfectionCase, 4, '4')
+    ImperfectionCase.Local(ImperfectionCase, 5, '5')
 
     MemberImperfectionDict = {
-        "imperfection_type"         : "IMPERFECTION_TYPE_INITIAL_BOW",
+        "imperfection_type"         : "IMPERFECTION_TYPE_INITIAL_BOW_AND_CRITERION",
+        "active_criterion"          : "ACTIVITY_CRITERION_ALWAYS",
         "imperfection_direction"    : "IMPERFECTION_DIRECTION_LOCAL_Y",
         "definition_type"           : "DEFINITION_TYPE_RELATIVE",
         "basic_value_relative"      : 300.0
@@ -78,6 +87,8 @@ if __name__ == '__main__':
     MemberImperfection(1, 1, '1', 'My Comment', MemberImperfectionDict)
     MemberImperfection.InitialSwayRelative(MemberImperfection, 2, 2, '1', 250.0, ImperfectionDirection.IMPERFECTION_DIRECTION_LOCAL_Y_NEGATIVE)
     MemberImperfection.InitialBowRelative(MemberImperfection,3, 3, '1', 200.0, ImperfectionDirection.IMPERFECTION_DIRECTION_LOCAL_Y_NEGATIVE)
+    MemberImperfection.InitialBowRelativeAndCriterion(MemberImperfection, 4, 4, '1', ImperfectionActivationCriterion.ACTIVITY_CRITERION_ALWAYS, [150.0])
+    MemberImperfection.InitialBowRelativeAndCriterion(MemberImperfection, 5, 5, '1', ImperfectionActivationCriterion.ACTIVITY_CRITERION_DEFINE, [150.0, 1.78])
 
 
     # Dieses Testprogramm wird noch etwas ausgebaut und schön gemacht. Dann wird es als
