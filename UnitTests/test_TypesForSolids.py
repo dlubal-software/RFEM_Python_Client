35e0dc0df30a806d5e1a41aedb85e8ea6d6c7afc
MichalO <ondrej.michal@dlubal.cz>
2022-05-13 06:48:15 +0200

    bug 27938 fixed

--- UnitTests/test_TypesForSolids.py
+++ UnitTests/test_TypesForSolids.py
@@ -27,11 +27,10 @@ def test_types_for_solids():
 
     SolidContact(1, SolidContactPerpendicularType.FAILURE_UNDER_COMPRESSION, SolidContactParallelType.FAILURE_IF_CONTACT_PERPENDICULAR_TO_SURFACES_FAILED)
     SolidContact(2, SolidContactPerpendicularType.FAILURE_UNDER_TENSION, SolidContactParallelType.FULL_FORCE_TRANSMISSION)
-    # TODO: bug 27938
     SolidContact(3, SolidContactPerpendicularType.FULL_FORCE_TRANSMISSION, SolidContactParallelType.RIGID_FRICTION, [2])
-    #SolidContact(4, SolidContactPerpendicularType.FAILURE_UNDER_COMPRESSION, SolidContactParallelType.RIGID_FRICTION_LIMIT, [10000000])
+    SolidContact(4, SolidContactPerpendicularType.FAILURE_UNDER_COMPRESSION, SolidContactParallelType.RIGID_FRICTION_LIMIT, [10000000])
     SolidContact(5, SolidContactPerpendicularType.FAILURE_UNDER_COMPRESSION, SolidContactParallelType.ELASTIC_FRICTION, [510, 0.5])
-    #SolidContact(6, SolidContactPerpendicularType.FAILURE_UNDER_COMPRESSION, SolidContactParallelType.ELASTIC_FRICTION_LIMIT, [520, 11000000])
+    SolidContact(6, SolidContactPerpendicularType.FAILURE_UNDER_COMPRESSION, SolidContactParallelType.ELASTIC_FRICTION_LIMIT, [520, 11000000])
     SolidContact(7, SolidContactPerpendicularType.FAILURE_UNDER_COMPRESSION, SolidContactParallelType.ELASTIC_SOLID, [530])
     with pytest.raises(ValueError):
         SolidContact(8, SolidContactPerpendicularType.FAILURE_UNDER_TENSION, SolidContactParallelType.FULL_FORCE_TRANSMISSION, [0.15])
@@ -55,21 +54,21 @@ def test_types_for_solids():
 
     contact = Model.clientModel.service.get_solid_contacts(3)
     assert contact.parallel_to_surface == SolidContactParallelType.RIGID_FRICTION.name
-    #assert round(contact.friction_coefficient, 2) == 0.5
+    assert round(contact.friction_coefficient, 2) == 2
 
-    #contact = Model.clientModel.service.get_solid_contacts(4)
-    #assert contact.parallel_to_surface == SolidContactParallelType.RIGID_FRICTION_LIMIT.name
-    #assert round(contact.limit_stress) == 10000000
+    contact = Model.clientModel.service.get_solid_contacts(4)
+    assert contact.parallel_to_surface == SolidContactParallelType.RIGID_FRICTION_LIMIT.name
+    assert round(contact.limit_stress) == 10000000
 
     contact = Model.clientModel.service.get_solid_contacts(5)
     assert contact.parallel_to_surface == SolidContactParallelType.ELASTIC_FRICTION.name
-    assert round(contact.shear_stiffness) == 510
-    #assert round(contact.friction_coefficient, 2) == 0.2
+    assert round(contact.shear_stiffness, 2) == 510
+    assert round(contact.friction_coefficient, 2) == 0.5
 
-    #contact = Model.clientModel.service.get_solid_contacts(6)
-    #assert contact.parallel_to_surface == SolidContactParallelType.ELASTIC_FRICTION_LIMIT.name
-    #assert round(contact.shear_stiffness) == 520
-    #assert round(contact.limit_stress) == 11000000
+    contact = Model.clientModel.service.get_solid_contacts(6)
+    assert contact.parallel_to_surface == SolidContactParallelType.ELASTIC_FRICTION_LIMIT.name
+    assert round(contact.shear_stiffness) == 520
+    assert round(contact.limit_stress) == 11000000
 
     contact = Model.clientModel.service.get_solid_contacts(7)
     assert contact.parallel_to_surface == SolidContactParallelType.ELASTIC_SOLID.name
