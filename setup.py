a53ee288c81af007aa3a20e560c7a44f34b81ebb
MichalO <ondrej.michal@dlubal.cz>
2022-09-29 14:23:52 +0200

    version update

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ readme = (here/"README.md").read_text(encoding="utf-8")
 
 setup(
     name='RFEM',
-    version='1.12.0',
+    version='1.13.0',
     description='Python Framework for RFEM6 Web Services',
     long_description=readme,
     long_description_content_type = "text/markdown",
