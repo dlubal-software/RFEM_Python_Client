92b21e65528c2558f4b146f4d8196bac4ad31179
matchristmann <matti.christmann@dlubal.com>
2022-10-10 17:01:40 +0200

    Update GetObjectNumbersByType.py
    Changed the way code checks if objects are available.

--- RFEM/Tools/GetObjectNumbersByType.py
+++ RFEM/Tools/GetObjectNumbersByType.py
@@ -19,56 +19,60 @@ class GetObjectNumbersByType():
         }
         """
 
-        modelStatus = GetModelInfo()
         ObjectDictionary = {}
 
         # Line Count
-        if modelStatus.property_line_count != 0:
 
-            objecttype = ObjectTypes.E_OBJECT_TYPE_LINE.name
-            ObjectNumber = Model.clientModel.service.get_all_object_numbers_by_type(objecttype)
+        ObjectType = ObjectTypes.E_OBJECT_TYPE_LINE.name
+        ObjectNumber = Model.clientModel.service.get_all_object_numbers_by_type(ObjectType)
+
+        if len(ObjectNumber) != 0:
+
             ObjectDictionary["Line"] = []
-
             for i in range(len(ObjectNumber.item)):
                 ObjectDictionary["Line"].append(ObjectNumber.item[i].no)
 
         # Member Count
-        if modelStatus.property_member_count != 0:
 
-            objecttype = ObjectTypes.E_OBJECT_TYPE_MEMBER.name
-            ObjectNumber = Model.clientModel.service.get_all_object_numbers_by_type(objecttype)
+        ObjectType = ObjectTypes.E_OBJECT_TYPE_MEMBER.name
+        ObjectNumber = Model.clientModel.service.get_all_object_numbers_by_type(ObjectType)
+
+        if len(ObjectNumber) != 0:
+
             ObjectDictionary["Member"] = []
-
             for i in range(len(ObjectNumber.item)):
                 ObjectDictionary["Member"].append(ObjectNumber.item[i].no)
 
         # Node Count
-        if modelStatus.property_node_count != 0:
 
-            objecttype = ObjectTypes.E_OBJECT_TYPE_NODE.name
-            ObjectNumber = Model.clientModel.service.get_all_object_numbers_by_type(objecttype)
+        ObjectType = ObjectTypes.E_OBJECT_TYPE_NODE.name
+        ObjectNumber = Model.clientModel.service.get_all_object_numbers_by_type(ObjectType)
+
+        if len(ObjectNumber) != 0:
+
             ObjectDictionary["Node"] = []
-
             for i in range(len(ObjectNumber.item)):
                 ObjectDictionary["Node"].append(ObjectNumber.item[i].no)
 
         # Section Count
-        if modelStatus.property_rsection_element_count != 0:
 
-            objecttype = ObjectTypes.E_OBJECT_TYPE_SECTION.name
-            ObjectNumber = Model.clientModel.service.get_all_object_numbers_by_type(objecttype)
+        ObjectType = ObjectTypes.E_OBJECT_TYPE_SECTION.name
+        ObjectNumber = Model.clientModel.service.get_all_object_numbers_by_type(ObjectType)
+        if len(ObjectNumber) != 0:
+
             ObjectDictionary["Section"] = []
-
             for i in range(len(ObjectNumber.item)):
                 ObjectDictionary["Section"].append(ObjectNumber.item[i].no)
 
         # Surface Count
-        if modelStatus.property_surface_count != 0:
 
-            objecttype = ObjectTypes.E_OBJECT_TYPE_SURFACE.name
-            ObjectNumber = Model.clientModel.service.get_all_object_numbers_by_type(objecttype)
+
+        ObjectType = ObjectTypes.E_OBJECT_TYPE_SURFACE.name
+        ObjectNumber = Model.clientModel.service.get_all_object_numbers_by_type(ObjectType)
+
+        if len(ObjectNumber) != 0:
+
             ObjectDictionary["Surface"] = []
-
             for i in range(len(ObjectNumber.item)):
                 ObjectDictionary["Surface"].append(ObjectNumber.item[i].no)
 
