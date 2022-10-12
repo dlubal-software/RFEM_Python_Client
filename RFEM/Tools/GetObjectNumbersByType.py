1df8b5e555f4f9d50507181352f21d348aabeece
matchristmann <matti.christmann@dlubal.com>
2022-10-12 15:06:29 +0200

    Updated GetObjectNumbersByType
    Update to different structure after OndraMichal's comment

--- RFEM/Tools/GetObjectNumbersByType.py
+++ RFEM/Tools/GetObjectNumbersByType.py
@@ -1,188 +1,25 @@
 from RFEM.initModel import Model
 from RFEM.enums import ObjectTypes
-from RFEM.Calculate.meshSettings import GetModelInfo
 
 class GetObjectNumbersByType():
 
     @staticmethod
-    def GetBasicObjects(model = Model):
+    def GetObjectNumbers(ObjectType = ObjectTypes.E_OBJECT_TYPE_NODE, model = Model):
+
+        """Returns a sorted list which contains object numbers in RFEM tables:
+
+            ObjectNumberList = [1, 2, ... ]
 
         """
-        Returns a dictionary which contains basic object numbers in RFEM tables.
 
-        ObjectDictionary = {
-            "Line": [],
-            "Line_Set": [],
-            "Material": [],
-            "Member": [],
-            "Member_Set": [],
-            "Node": [],
-            "Opening": [],
-            "Section": [],
-            "Solid": [],
-            "Solid_Set": [],
-            "Surface": []
-            "Surface_Set": [],
-            "Thickness": [],
-        }
-        """
-
-        ObjectDictionary = {}
-
-        # Line Count
-
-        ObjectType = ObjectTypes.E_OBJECT_TYPE_LINE.name
-        ObjectNumber = Model.clientModel.service.get_all_object_numbers_by_type(ObjectType)
+        ObjectNumber = Model.clientModel.service.get_all_object_numbers_by_type(ObjectType.name)
 
         if len(ObjectNumber) != 0:
-
-            ObjectDictionary["Line"] = []
+            ObjectNumberList = []
             for i in range(len(ObjectNumber.item)):
-                ObjectDictionary["Line"].append(ObjectNumber.item[i].no)
-            ObjectDictionary["Line"].sort()
+                ObjectNumberList.append(ObjectNumber.item[i].no)
+            ObjectNumberList.sort()
+        else:
+            None
 
-        # Line_Set Count
-
-        ObjectType = ObjectTypes.E_OBJECT_TYPE_LINE_SET.name
-        ObjectNumber = Model.clientModel.service.get_all_object_numbers_by_type(ObjectType)
-
-        if len(ObjectNumber) != 0:
-
-            ObjectDictionary["Line_Set"] = []
-            for i in range(len(ObjectNumber.item)):
-                ObjectDictionary["Line_Set"].append(ObjectNumber.item[i].no)
-            ObjectDictionary["Line_Set"].sort()
-
-        # Material Count
-
-        ObjectType = ObjectTypes.E_OBJECT_TYPE_MATERIAL.name
-        ObjectNumber = Model.clientModel.service.get_all_object_numbers_by_type(ObjectType)
-
-        if len(ObjectNumber) != 0:
-
-            ObjectDictionary["Material"] = []
-            for i in range(len(ObjectNumber.item)):
-                ObjectDictionary["Material"].append(ObjectNumber.item[i].no)
-            ObjectDictionary["Material"].sort()
-
-        # Member Count
-
-        ObjectType = ObjectTypes.E_OBJECT_TYPE_MEMBER.name
-        ObjectNumber = Model.clientModel.service.get_all_object_numbers_by_type(ObjectType)
-
-        if len(ObjectNumber) != 0:
-
-            ObjectDictionary["Member"] = []
-            for i in range(len(ObjectNumber.item)):
-                ObjectDictionary["Member"].append(ObjectNumber.item[i].no)
-            ObjectDictionary["Member"].sort()
-
-        # Member_Set Count
-
-        ObjectType = ObjectTypes.E_OBJECT_TYPE_MEMBER_SET.name
-        ObjectNumber = Model.clientModel.service.get_all_object_numbers_by_type(ObjectType)
-
-        if len(ObjectNumber) != 0:
-
-            ObjectDictionary["Member_Set"] = []
-            for i in range(len(ObjectNumber.item)):
-                ObjectDictionary["Member_Set"].append(ObjectNumber.item[i].no)
-            ObjectDictionary["Member_Set"].sort()
-
-        # Node Count
-
-        ObjectType = ObjectTypes.E_OBJECT_TYPE_NODE.name
-        ObjectNumber = Model.clientModel.service.get_all_object_numbers_by_type(ObjectType)
-
-        if len(ObjectNumber) != 0:
-
-            ObjectDictionary["Node"] = []
-            for i in range(len(ObjectNumber.item)):
-                ObjectDictionary["Node"].append(ObjectNumber.item[i].no)
-            ObjectDictionary["Node"].sort()
-
-        # Opening Count
-
-        ObjectType = ObjectTypes.E_OBJECT_TYPE_OPENING.name
-        ObjectNumber = Model.clientModel.service.get_all_object_numbers_by_type(ObjectType)
-
-        if len(ObjectNumber) != 0:
-
-            ObjectDictionary["Opening"] = []
-            for i in range(len(ObjectNumber.item)):
-                ObjectDictionary["Opening"].append(ObjectNumber.item[i].no)
-            ObjectDictionary["Opening"].sort()
-
-        # Section Count
-
-        ObjectType = ObjectTypes.E_OBJECT_TYPE_SECTION.name
-        ObjectNumber = Model.clientModel.service.get_all_object_numbers_by_type(ObjectType)
-        if len(ObjectNumber) != 0:
-
-            ObjectDictionary["Section"] = []
-            for i in range(len(ObjectNumber.item)):
-                ObjectDictionary["Section"].append(ObjectNumber.item[i].no)
-            ObjectDictionary["Section"].sort()
-
-        # Solid Count
-
-        ObjectType = ObjectTypes.E_OBJECT_TYPE_SOLID.name
-        ObjectNumber = Model.clientModel.service.get_all_object_numbers_by_type(ObjectType)
-
-        if len(ObjectNumber) != 0:
-
-            ObjectDictionary["Solid"] = []
-            for i in range(len(ObjectNumber.item)):
-                ObjectDictionary["Solid"].append(ObjectNumber.item[i].no)
-            ObjectDictionary["Solid"].sort()
-
-        # Solid_Set Count
-
-        ObjectType = ObjectTypes.E_OBJECT_TYPE_SOLID_SET.name
-        ObjectNumber = Model.clientModel.service.get_all_object_numbers_by_type(ObjectType)
-
-        if len(ObjectNumber) != 0:
-
-            ObjectDictionary["Solid_Set"] = []
-            for i in range(len(ObjectNumber.item)):
-                ObjectDictionary["Solid_Set"].append(ObjectNumber.item[i].no)
-            ObjectDictionary["Solid_Set"].sort()
-
-        # Surface Count
-
-        ObjectType = ObjectTypes.E_OBJECT_TYPE_SURFACE.name
-        ObjectNumber = Model.clientModel.service.get_all_object_numbers_by_type(ObjectType)
-
-        if len(ObjectNumber) != 0:
-
-            ObjectDictionary["Surface"] = []
-            for i in range(len(ObjectNumber.item)):
-                ObjectDictionary["Surface"].append(ObjectNumber.item[i].no)
-            ObjectDictionary["Surface"].sort()
-
-        # Surface_Set Count
-
-        ObjectType = ObjectTypes.E_OBJECT_TYPE_SURFACE_SET.name
-        ObjectNumber = Model.clientModel.service.get_all_object_numbers_by_type(ObjectType)
-
-        if len(ObjectNumber) != 0:
-
-            ObjectDictionary["Surface_Set"] = []
-            for i in range(len(ObjectNumber.item)):
-                ObjectDictionary["Surface_Set"].append(ObjectNumber.item[i].no)
-            ObjectDictionary["Surface_Set"].sort()
-
-        # Thickness Count
-
-        ObjectType = ObjectTypes.E_OBJECT_TYPE_THICKNESS.name
-        ObjectNumber = Model.clientModel.service.get_all_object_numbers_by_type(ObjectType)
-
-        if len(ObjectNumber) != 0:
-
-            ObjectDictionary["Thickness"] = []
-            for i in range(len(ObjectNumber.item)):
-                ObjectDictionary["Thickness"].append(ObjectNumber.item[i].no)
-            ObjectDictionary["Thickness"].sort()
-
-        return ObjectDictionary
-
+        return ObjectNumberList
--- UnitTests/test_GetObjectNumbersByType.py
+++ UnitTests/test_GetObjectNumbersByType.py
@@ -48,88 +48,31 @@ def test_GetObjectNumbersByType():
     Node(1, 0, 0, 0)
     Node(2, 5, 0, 0)
     Node(3, 0, 5, 0)
-    Node(4, 5, 5, 0)
-
-    Node(5, 1, 1, 0)
-    Node(6, 4, 1, 0)
-    Node(7, 4, 4, 0)
-    Node(8, 1, 4, 0)
-
-    Node(9, 0, 0, 1)
-    Node(10, 5, 0, 1)
-    Node(11, 0, 5, 1)
-    Node(12, 5, 5, 1)
-
-    Node(13, 0, 0, 2)
-    Node(14, 5, 0, 2)
-    Node(15, 0, 5, 2)
-    Node(16, 5, 5, 2)
 
 
     Line(1, '1 2')
-    Line(2, '2 4')
-    Line(3, '3 4')
-    Line(4, '1 3')
+    Line(2, '2 3')
     LineSet(1, '1 2')
 
-    Line(5, '5 6')
-    Line(6, '6 7')
-    Line(7, '7 8')
-    Line(8, '8 5')
-    LineSet(2, '5 6 7 8' )
 
-    Line(9, '9 10')
-    Line(10, '9 11')
-    Line(11, '11 12')
-    Line(12, '12 10')
-    LineSet(3, '9 10 11 12')
-
-    Line(13, '13 14')
-    Line(14, '13 15')
-    Line(15, '15 16')
-    Line(16, '16 14')
-    LineSet(4, '13 14 15 16')
-
-    Line(17, '9 13')
-    Line(18, '10 14')
-    Line(19, '11 15')
-    Line(20, '12 16')
-
-
-
-    Material(1, 'S235')
-
-    Section(1, 'IPE 300', 1)
-
-
-    Member(1, 1, 2, 0, 1, 1,)
-    Member(2, 2, 4, 0, 1, 1,)
-    MemberSet(1, '1 2')
-
-
-    Surface(1, '1 2 3 4', 1)
-
-    Surface(2, '9 10 11 12', 1)
-    Surface(3, '13 14 15 16', 1)
-    Surface(4, '11 15 19 20', 1)
-    Surface(5, '12 16 18 20', 1)
-    Surface(6, '9 13 17 18', 1)
-    Surface(7, '10 14 17 19', 1)
-    SurfaceSet(1, '2 3 4 5 6 7')
-
-    Opening(1, '5 6 7 8')
-
-    Solid(1, '2 3 4 5 6 7')
-    SolidSet(1, '1')
-
-    Thickness(1, '', 1)
 
     Model.clientModel.service.finish_modification()
 
 
-    ObjectDictionary = GetObjectNumbersByType.GetBasicObjects()
+    ObjectDictionary = GetObjectNumbersByType.GetObjectNumbers(ObjectTypes.E_OBJECT_TYPE_LINE)
+
+    assert ObjectDictionary == [1, 2]
+
+    ObjectDictionary = GetObjectNumbersByType.GetObjectNumbers(ObjectTypes.E_OBJECT_TYPE_NODE)
+
+    assert ObjectDictionary == [1, 2, 3]
+
+    ObjectDictionary = GetObjectNumbersByType.GetObjectNumbers(ObjectTypes.E_OBJECT_TYPE_LINE_SET)
+
+    assert ObjectDictionary == [1]
 
 
+"""
     assert ObjectDictionary["Line"] == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22]
 
     assert ObjectDictionary['Line_Set'] == [1, 2, 3, 4]
@@ -156,4 +99,4 @@ def test_GetObjectNumbersByType():
 
     assert ObjectDictionary["Thickness"] == [1]
 
-
+"""
