1b99921c115b0b4b9d9fc604db65f20bbbfb36f1
Björn Steinhagen <bjoern.steinhagen@dlubal.com>
2022-11-08 11:32:53 +0100

    Minor Cosmetic Changes

--- RFEM/dependencies.py
+++ RFEM/dependencies.py
@@ -15,7 +15,7 @@ try:
     import mock
     import suds.transport
     import suds.client # suds-py3
-    import pandas
+
 except:
     print('One of the required modules is not installed in your Python env.')
     instSUDS = input('\nDo you want to install all dependencies and check all their versions (y/n)? ')
@@ -26,10 +26,10 @@ except:
         import subprocess
         try:
             subprocess.call('python -m pip install --upgrade pip')
-            subprocess.call('python -m pip install requests six suds-py3 xmltodict pytest mock pandas --user')
+            subprocess.call('python -m pip install requests six suds-py3 xmltodict pytest mock --user')
         except:
             print('WARNING: Installation of modules failed!')
-            print('Please use command "python -m pip install requests six suds-py3 xmltodict pytest mock pandas --user" in your Command Prompt.')
+            print('Please use command "python -m pip install requests six suds-py3 xmltodict pytest mock --user" in your Command Prompt.')
             input('Press Enter to exit...')
             sys.exit()
     else:
