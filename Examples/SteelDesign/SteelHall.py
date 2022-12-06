e8b8a2e0875cf562f92a8cec49d641db3e959372
heetpatel96 <heetpatel96@gmail.com>
2022-12-06 16:46:59 +0100

    Update SteelHall.py

--- Examples/SteelDesign/SteelHall.py
+++ Examples/SteelDesign/SteelHall.py
@@ -44,7 +44,8 @@ if __name__ == '__main__':
     SetAddonStatus(Model.clientModel, AddOn.steel_design_active)
     Material(1, 'S235')
     Material(3)
-    Section(1, 'IPE 200', 1)
+    Section(1, 'IPE 500', 1)
+    Section(2, 'IPE 300', 1)
     Section(3, 'L 20x20x3', 3)
     i = 0
     for j in range(frame_number):
@@ -79,7 +80,7 @@ if __name__ == '__main__':
 
         m, n, o = i, k+1, k+10
         for l in range(5):
-            Member(m, n, o, 0, 1, 1)
+            Member(m, n, o, 0, 2, 2)
             m, n, o = m+1, n+1, o+1
         i, k = i+13, k+9
 
@@ -93,10 +94,10 @@ if __name__ == '__main__':
         if bracingV1.lower() == 'yes' or bracingV1.lower() == 'y':
             k = 1
             for j in range(frame_number-1):
-                Member(i+1, k, k+11, 0, 3, 3)
-                Member(i+2, k+2, k+9, 0, 3, 3)
-                Member(i+3, k+6, k+13, 0, 3, 3)
-                Member(i+4, k+4, k+15, 0, 3, 3)
+                Member.Tension(i+1, k, k+11, section_no= 3)
+                Member.Tension(i+2, k+2, k+9, section_no= 3)
+                Member.Tension(i+3, k+6, k+13, section_no= 3)
+                Member.Tension(i+4, k+4, k+15, section_no= 3)
                 i, k = i+4, k+9
 
         else:
@@ -106,10 +107,10 @@ if __name__ == '__main__':
             k = 1
             for j in range(frame_number-1):
                 if j in [0, frame_number-2]:
-                    Member(i+1, k, k+11, 0, 3, 3)
-                    Member(i+2, k+2, k+9, 0, 3, 3)
-                    Member(i+3, k+6, k+13, 0, 3, 3)
-                    Member(i+4, k+4, k+15, 0, 3, 3)
+                    Member.Tension(i+1, k, k+11, section_no= 3)
+                    Member.Tension(i+2, k+2, k+9, section_no= 3)
+                    Member.Tension(i+3, k+6, k+13, section_no= 3)
+                    Member.Tension(i+4, k+4, k+15, section_no= 3)
                     i = i+4
                 k = k+9
         elif bracingV2.lower() == 'no' or bracingV2.lower() == 'n':
@@ -119,10 +120,10 @@ if __name__ == '__main__':
             k = 1
             for j in range(frame_number-1):
                 if j % 2 != 0:
-                    Member(i+1, k, k+11, 0, 3, 3)
-                    Member(i+2, k+2, k+9, 0, 3, 3)
-                    Member(i+3, k+6, k+13, 0, 3, 3)
-                    Member(i+4, k+4, k+15, 0, 3, 3)
+                    Member.Tension(i+1, k, k+11, section_no= 3)
+                    Member.Tension(i+2, k+2, k+9, section_no= 3)
+                    Member.Tension(i+3, k+6, k+13, section_no= 3)
+                    Member.Tension(i+4, k+4, k+15, section_no= 3)
                     i = i + 4
                 k = k+9
 
@@ -130,10 +131,10 @@ if __name__ == '__main__':
             k = 1
             for j in range(frame_number-1):
                 if j % 2 == 0:
-                    Member(i+1, k, k+11, 0, 3, 3)
-                    Member(i+2, k+2, k+9, 0, 3, 3)
-                    Member(i+3, k+6, k+13, 0, 3, 3)
-                    Member(i+4, k+4, k+15, 0, 3, 3)
+                    Member.Tension(i+1, k, k+11, section_no= 3)
+                    Member.Tension(i+2, k+2, k+9, section_no= 3)
+                    Member.Tension(i+3, k+6, k+13, section_no= 3)
+                    Member.Tension(i+4, k+4, k+15, section_no= 3)
                     i = i + 4
                 k = k+9
 
@@ -143,29 +144,29 @@ if __name__ == '__main__':
         if bracingV.lower() == 'no' or bracingV.lower() == 'n':
             k = 1
             for j in range(frame_number-1):
-                Member(i+1, k+2, k+12, 0, 3, 3)
-                Member(i+2, k+3, k+11, 0, 3, 3)
-                Member(i+3, k+3, k+13, 0, 3, 3)
-                Member(i+4, k+4, k+12, 0, 3, 3)
+                Member.Tension(i+1, k+2, k+12, section_no= 3)
+                Member.Tension(i+2, k+3, k+11, section_no= 3)
+                Member.Tension(i+3, k+3, k+13, section_no= 3)
+                Member.Tension(i+4, k+4, k+12, section_no= 3)
                 i, k = i+4, k+9
 
         if bracingV1.lower() == 'yes' or bracingV1.lower() == 'y':
             k = 1
             for j in range(frame_number-1):
-                Member(i+1, k+2, k+12, 0, 3, 3)
-                Member(i+2, k+3, k+11, 0, 3, 3)
-                Member(i+3, k+3, k+13, 0, 3, 3)
-                Member(i+4, k+4, k+12, 0, 3, 3)
+                Member.Tension(i+1, k+2, k+12, section_no= 3)
+                Member.Tension(i+2, k+3, k+11, section_no= 3)
+                Member.Tension(i+3, k+3, k+13, section_no= 3)
+                Member.Tension(i+4, k+4, k+12, section_no= 3)
                 i, k = i+4, k+9
 
         if bracingV2.lower() == 'yes' or bracingV2.lower() == 'y':
             k = 1
             for j in range(frame_number-1):
                 if j in [0, frame_number-2]:
-                    Member(i+1, k+2, k+12, 0, 3, 3)
-                    Member(i+2, k+3, k+11, 0, 3, 3)
-                    Member(i+3, k+3, k+13, 0, 3, 3)
-                    Member(i+4, k+4, k+12, 0, 3, 3)
+                    Member.Tension(i+1, k+2, k+12, section_no= 3)
+                    Member.Tension(i+2, k+3, k+11, section_no= 3)
+                    Member.Tension(i+3, k+3, k+13, section_no= 3)
+                    Member.Tension(i+4, k+4, k+12, section_no= 3)
                     i = i+4
                 k = k+9
 
@@ -173,10 +174,10 @@ if __name__ == '__main__':
             k = 1
             for j in range(frame_number-1):
                 if j % 2 != 0:
-                    Member(i+1, k+2, k+12, 0, 3, 3)
-                    Member(i+2, k+3, k+11, 0, 3, 3)
-                    Member(i+3, k+3, k+13, 0, 3, 3)
-                    Member(i+4, k+4, k+12, 0, 3, 3)
+                    Member.Tension(i+1, k+2, k+12, section_no= 3)
+                    Member.Tension(i+2, k+3, k+11, section_no= 3)
+                    Member.Tension(i+3, k+3, k+13, section_no= 3)
+                    Member.Tension(i+4, k+4, k+12, section_no= 3)
                     i = i + 4
                 k = k+9
 
@@ -184,10 +185,10 @@ if __name__ == '__main__':
             k = 1
             for j in range(frame_number-1):
                 if j % 2 == 0:
-                    Member(i+1, k+2, k+12, 0, 3, 3)
-                    Member(i+2, k+3, k+11, 0, 3, 3)
-                    Member(i+3, k+3, k+13, 0, 3, 3)
-                    Member(i+4, k+4, k+12, 0, 3, 3)
+                    Member.Tension(i+1, k+2, k+12, section_no= 3)
+                    Member.Tension(i+2, k+3, k+11, section_no= 3)
+                    Member.Tension(i+3, k+3, k+13, section_no= 3)
+                    Member.Tension(i+4, k+4, k+12, section_no= 3)
                     i = i + 4
                 k = k+9
 
@@ -204,7 +205,7 @@ if __name__ == '__main__':
     StaticAnalysisSettings.SecondOrderPDelta(2, "SecondOrder")
     StaticAnalysisSettings.LargeDeformation(3, "LargeDeformation")
 
-    LoadCase(1, 'Self-Weight', [True, 0.0, 0.0, 1.0])
+    LoadCase.StaticAnalysis(1, 'Self-Weight', True, 1, ActionCategoryType.ACTION_CATEGORY_PERMANENT_G, [True, 0.0, 0.0, 1.0])
     LoadCase.StaticAnalysis(2, 'Live Load', True, 1, ActionCategoryType.ACTION_CATEGORY_IMPOSED_LOADS_CATEGORY_H_ROOFS_QI_H, [False])
     LoadCase.StaticAnalysis(3, 'Snow-Load', True, 1, ActionCategoryType.ACTION_CATEGORY_SNOW_ICE_LOADS_H_LESS_OR_EQUAL_TO_1000_M_QS, [False])
     LoadCase.StaticAnalysis(4, 'Wind-Load_x', True, 1, ActionCategoryType.ACTION_CATEGORY_WIND_QW, [False])
@@ -213,7 +214,9 @@ if __name__ == '__main__':
     LoadCombination(1, AnalysisType.ANALYSIS_TYPE_STATIC, 1, '', 2, False, False, False, True, [[1,1,0,False],[1,2,0,False]])
     LoadCombination(2, AnalysisType.ANALYSIS_TYPE_STATIC, 1, '', 2, False, False, False, True, [[1,1,0,False],[1,3,0,False]])
     LoadCombination(3, AnalysisType.ANALYSIS_TYPE_STATIC, 1, '', 2, False, False, False, True, [[1,1,0,False],[1,4,0,False]])
-    LoadCombination(4, AnalysisType.ANALYSIS_TYPE_STATIC, 1, '', 2, False, False, False, True, [[1,1,0,False],[1,2,0,False],[1,3,0,False],[1,4,0,False]])
+    LoadCombination(4, AnalysisType.ANALYSIS_TYPE_STATIC, 1, '', 2, False, False, False, True, [[1,1,0,False],[1,5,0,False]])
+    LoadCombination(5, AnalysisType.ANALYSIS_TYPE_STATIC, 1, '', 2, False, False, False, True, [[1,1,0,False],[1,2,0,False],[1,3,0,False],[1,4,0,False]])
+    LoadCombination(6, AnalysisType.ANALYSIS_TYPE_STATIC, 1, '', 2, False, False, False, True, [[1,1,0,False],[1,2,0,False],[1,3,0,False],[1,5,0,False]])
 
     # n,k = 0,0
     # for j in range(frame_number):
