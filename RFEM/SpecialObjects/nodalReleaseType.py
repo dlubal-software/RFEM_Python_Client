40824bd289c6b5bfcc6284ad88688049d3b78063
alicia-knauer <alicia.knauer@gmx.de>
2023-03-28 14:17:58 +0200

    Update nodalReleaseType.py

--- RFEM/SpecialObjects/nodalReleaseType.py
+++ RFEM/SpecialObjects/nodalReleaseType.py
@@ -43,7 +43,9 @@ class NodalReleaseType():
                         negative/positive zone = [negative/positive zone type, force, slippage]
                 for translational_release_n/vy/vz_nonlinearity[0] == NodalReleaseTypeReleaseNonlinearity.NONLINEARITY_TYPE_DIAGRAM:
                     translational_release_n/vy/vz_nonlinearity = [nonlinearity type Diagram, [symmetric(bool), NodalReleaseTypeDiagram Enumeration(start), NodalReleaseTypeDiagram Enumeration(end)], [[displacement, force],...]]
-            rotational_release_mt/my/mz_nonlinearity (list/list of lists): Nonlinearity Parameter for Rotational Release around X Direction
+            rotational_release_mt_nonlinearity (list/list of lists): Nonlinearity Parameter for Rotational Release around X Direction
+            rotational_release_my_nonlinearity (list/list of lists): Nonlinearity Parameter for Rotational Release around Y Direction
+            rotational_release_mz_nonlinearity (list/list of lists): Nonlinearity Parameter for Rotational Release around Z Direction
                 for rotational_release_mt/my/mz_nonlinearity[0] == NodalReleaseTypePartialActivityAround.NONLINEARITY_TYPE_PARTIAL_ACTIVITY:
                     rotational_release_mt/my/mz_nonlinearity = [nonlinearity type Partial_Activity, negative zone, positive zone]
                     for negative/positive zone[0] == NodalReleaseTypePartialActivityAround.PARTIAL_ACTIVITY_TYPE_COMPLETE:
