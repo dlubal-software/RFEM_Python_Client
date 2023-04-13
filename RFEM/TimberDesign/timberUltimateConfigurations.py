47c5901b0db1f842bbea31a8da8dd6c231ecb9f9
heetpatel96 <heetpatel96@gmail.com>
2023-04-13 15:00:01 +0200

    update in docstrings
    I did minor corrections in docstrings and removed default values from parameters which are auto calculated from material and section properties in TypesForMembers.

--- RFEM/TimberDesign/timberUltimateConfigurations.py
+++ RFEM/TimberDesign/timberUltimateConfigurations.py
@@ -10,10 +10,11 @@ class TimberDesignUltimateConfigurations():
                  comment: str = '',
                  params: dict = None,
                  model = Model):
+
         """
         Args:
             no (int): Timber Design Ultimate Configuration Tag
-            user_defined_name (list): User Defined Configuration Name
+            name (list): User Defined Configuration Name
             members_no (str): Assign Configuration to Selected Members
             member_sets_no (str): Assign Configuration to Selected Member Sets
             comment (str, optional): Comment
@@ -64,4 +65,3 @@ class TimberDesignUltimateConfigurations():
 
         # Add Global Parameters to Client Model
         model.clientModel.service.set_timber_design_uls_configuration(clientObject)
-
