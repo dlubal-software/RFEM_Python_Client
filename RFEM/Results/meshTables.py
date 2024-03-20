af556341b2a16ea5e1b46d8ec1ee1b82821d10ee
heetpatel96 <heetpatel96@gmail.com>
2024-03-20 17:01:59 +0100

    Update meshTables.py

--- RFEM/Results/meshTables.py
+++ RFEM/Results/meshTables.py
@@ -1,5 +1,6 @@
 from RFEM.initModel import Model
 from RFEM.Results.resultTables import ConvertResultsToListOfDct
+from RFEM.enums import CaseObjectType
 
 class MeshTables():
 
@@ -9,64 +10,154 @@ class MeshTables():
         model = Model):
         '''
         Args:
-            model(class, optional): Model instance
+            model (class, optional): Model instance
         '''
         return ConvertResultsToListOfDct(model.clientModel.service.get_all_FE_nodes_original_mesh(), include_base)
 
     @staticmethod
-    def getAllFE1DElements(
+    def GetAllFE1DElements(
         include_base: bool = False,
         model = Model):
         '''
         Args:
-            model(class, optional): Model instance
+            model (class, optional): Model instance
         '''
         return ConvertResultsToListOfDct(model.clientModel.service.get_all_FE_1D_elements(), include_base)
 
     @staticmethod
-    def getAllFE2DElements(
+    def GetAllFE2DElements(
         include_base: bool = False,
         model = Model):
         '''
         Args:
-            model(class, optional): Model instance
+            model (class, optional): Model instance
         '''
         return ConvertResultsToListOfDct(model.clientModel.service.get_all_FE_2D_elements(), include_base)
 
     @staticmethod
-    def getAllFE3DElements(
+    def GetAllFE3DElements(
         include_base: bool = False,
         model = Model):
         '''
         Args:
-            model(class, optional): Model instance
+            model (class, optional): Model instance
         '''
         return ConvertResultsToListOfDct(model.clientModel.service.get_all_FE_3D_elements(), include_base)
 
     @staticmethod
-    def getFENodeOriginalMesh(
+    def GetFENodeOriginalMesh(
         nodeNo: int = 1,
         model = Model):
+        '''
+        Args:
+            nodeNo (int): Node Number
+            model (class, optional): Model instance
+        '''
 
         return model.clientModel.service.get_FE_node_original_mesh(nodeNo)
 
     @staticmethod
-    def getFE1DElement(
+    def GetFE1DElement(
         elementNo: int = 1,
         model = Model):
+        '''
+        Args:
+            elementNo (int): Element Number
+            model (class, optional): Model instance
+        '''
 
         return model.clientModel.service.get_FE_1D_element(elementNo)
 
     @staticmethod
-    def getFE2DElement(
+    def GetFE2DElement(
         elementNo: int = 1,
         model = Model):
+        '''
+        Args:
+            elementNo (int): Element Number
+            model (class, optional): Model instance
+        '''
 
         return model.clientModel.service.get_FE_2D_element(elementNo)
 
     @staticmethod
-    def getFE3DElement(
+    def GetFE3DElement(
         elementNo: int = 1,
         model = Model):
+        '''
+        Args:
+            elementNo (int): Element Number
+            model (class, optional): Model instance
+        '''
 
         return model.clientModel.service.get_FE_3D_element(elementNo)
+
+    @staticmethod
+    def GetAllFENodesInitialState(
+        loading_type = CaseObjectType.E_OBJECT_TYPE_LOAD_CASE,
+        loading_no: int = 1,
+        increment_number: int = 1,
+        include_base: bool = False,
+        model = Model):
+        '''
+        Args:
+            loading_type (emun): Case Object Loading Type Enumeration
+            loading_no (int): Loading Number
+            increment_number (int): Increment Number
+            model (class, optional): Model instance
+        '''
+
+        return ConvertResultsToListOfDct(model.clientModel.service.get_all_FE_nodes_initial_state_mesh({'case_object': {'type': loading_type.name, 'id': loading_no}, 'increment_number': increment_number}), include_base)
+
+    @staticmethod
+    def GetAllFENodesDeformed(
+        loading_type = CaseObjectType.E_OBJECT_TYPE_LOAD_CASE,
+        loading_no: int = 1,
+        increment_number: int = 1,
+        include_base: bool = False,
+        model = Model):
+        '''
+        Args:
+            loading_type (emun): Case Object Loading Type Enumeration
+            loading_no (int): Loading Number
+            increment_number (int): Increment Number
+            model (class, optional): Model instance
+        '''
+
+        return ConvertResultsToListOfDct(model.clientModel.service.get_all_FE_nodes_deformed_mesh({'case_object': {'type': loading_type.name, 'id': loading_no}, 'increment_number': increment_number}), include_base)
+
+    @staticmethod
+    def GetFENodeInitialState(
+        nodeNo:int = 1,
+        loading_type = CaseObjectType.E_OBJECT_TYPE_LOAD_CASE,
+        loading_no: int = 1,
+        increment_number: int = 1,
+        model = Model):
+        '''
+        Args:
+            nodeNo (int): Node Number
+            loading_type (emun): Case Object Loading Type Enumeration
+            loading_no (int): Loading Number
+            increment_number (int): Increment Number
+            model (class, optional): Model instance
+        '''
+
+        return model.clientModel.service.get_FE_node_initial_state_mesh(nodeNo, {'case_object': {'type': loading_type.name, 'id': loading_no}, 'increment_number': increment_number})
+
+    @staticmethod
+    def GetFENodeDeformed(
+        nodeNo:int = 1,
+        loading_type = CaseObjectType.E_OBJECT_TYPE_LOAD_CASE,
+        loading_no: int = 1,
+        increment_number: int = 1,
+        model = Model):
+        '''
+        Args:
+            nodeNo (int): Node Number
+            loading_type (emun): Case Object Loading Type Enumeration
+            loading_no (int): Loading Number
+            increment_number (int): Increment Number
+            model (class, optional): Model instance
+        '''
+
+        return model.clientModel.service.get_FE_node_deformed_mesh(nodeNo, {'case_object': {'type': loading_type.name, 'id': loading_no}, 'increment_number': increment_number})
