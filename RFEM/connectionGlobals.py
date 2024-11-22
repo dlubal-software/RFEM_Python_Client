e70f07f3ca4ad4ff6b0217d900354edd4d7496d2
jarabroz <jara.broz@live.com>
2024-11-22 11:23:03 +0100

    Revert "package number fix"
    This reverts commit f81d53203475805f87bb3ad8f24ef3c5fa2666f3.

--- RFEM/connectionGlobals.py
+++ RFEM/connectionGlobals.py
@@ -1,9 +1,9 @@
 
-url = "https://20.33.49.35"
+url = "http://127.0.0.1"
 port = "8081"
 connected = False
-api_key = "AK-387062-Hp1fc0Fk8wXyNPnkTOGsTDpigOwt0ngr"
-verify = r"C:/WORK/REPOSITORIES/Dlubal-WebService-GitHub/RFEM_Python_Client/DlubalWebServiceServer.crt" # This can be a boolean or a path to a CA bundle file (in case of self-signed certificate it's required)
+api_key = None
+verify = True # This can be a boolean or a path to a CA bundle file (in case of self-signed certificate it's required)
 
 # Is filled in the initModel
 client = None
