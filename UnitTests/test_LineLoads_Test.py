e243b865ddeb8effd8b3be5da004fd639e3973da
MichalO <ondrej.michal@dlubal.cz>
2025-12-18 12:20:18 +0100

    update

--- UnitTests/test_LineLoads_Test.py
+++ UnitTests/test_LineLoads_Test.py
@@ -119,13 +119,13 @@ def test_line_loads():
     LineLoad.Force(6, 2, '6',
                    load_distribution=LineLoadDistribution.LOAD_DISTRIBUTION_CONCENTRATED_VARYING,
                    load_parameter=[[1.5, 200], [2, 200]])
-    """
+
     ll = Model.clientModel.service.get_line_load(6, 2)
     assert ll.lines == '6'
     assert ll.load_distribution == 'LOAD_DISTRIBUTION_CONCENTRATED_VARYING'
     assert ll.varying_load_parameters['line_load_varying_load_parameters'][0].row['distance'] == 1.5
     assert ll.varying_load_parameters['line_load_varying_load_parameters'][1].row['magnitude'] == 200
-    """
+
     LineLoad.Force(7, 2, '7',
                    load_distribution=LineLoadDistribution.LOAD_DISTRIBUTION_TRAPEZOIDAL,
                    load_parameter=[True, True, 2000, 2000, 0.2, 0.5])
@@ -150,13 +150,13 @@ def test_line_loads():
     LineLoad.Force(9, 2, '9',
                    load_distribution=LineLoadDistribution.LOAD_DISTRIBUTION_VARYING,
                    load_parameter=[[1.25, 75000], [2, 60000]])
-    """
+
     ll = Model.clientModel.service.get_line_load(9, 2)
     assert ll.lines == '9'
     assert ll.load_distribution == 'LOAD_DISTRIBUTION_VARYING'
     assert ll.varying_load_parameters['line_load_varying_load_parameters'][1].row['magnitude'] == 60000
     assert ll.varying_load_parameters['line_load_varying_load_parameters'][0].row['distance'] == 1.25
-    """
+
     #   TESTING MOMENT TYPE LINE LOADS
 
     LoadCase(3, 'TYPE: Moment')
@@ -220,13 +220,13 @@ def test_line_loads():
                     load_distribution=LineLoadDistribution.LOAD_DISTRIBUTION_CONCENTRATED_VARYING,
                     load_direction=NodalLoadDirection.LOAD_DIRECTION_LOCAL_X,
                     load_parameter=[[1.23, 200], [2.67, 200]])
-    """
+
     ll = Model.clientModel.service.get_line_load(6, 3)
     assert ll.lines == '6'
     assert ll.load_distribution == 'LOAD_DISTRIBUTION_CONCENTRATED_VARYING'
     assert ll.varying_load_parameters['line_load_varying_load_parameters'][0].row['distance'] == 1.23
     assert ll.varying_load_parameters['line_load_varying_load_parameters'][1].row['magnitude'] == 200
-    """
+
     LineLoad.Moment(7, 3, '7',
                     load_distribution=LineLoadDistribution.LOAD_DISTRIBUTION_TRAPEZOIDAL,
                     load_direction=NodalLoadDirection.LOAD_DIRECTION_LOCAL_X,
@@ -265,13 +265,13 @@ def test_line_loads():
                     load_distribution=LineLoadDistribution.LOAD_DISTRIBUTION_VARYING,
                     load_direction=NodalLoadDirection.LOAD_DIRECTION_LOCAL_X,
                     load_parameter=[[0.96, 75000], [2.34, 60000]])
-    """
+
     ll = Model.clientModel.service.get_line_load(10, 3)
     assert ll.lines == '10'
     assert ll.load_distribution == 'LOAD_DISTRIBUTION_VARYING'
     assert ll.varying_load_parameters['line_load_varying_load_parameters'][1].row['magnitude'] == 60000
     assert ll.varying_load_parameters['line_load_varying_load_parameters'][0].row['distance'] == 0.96
-    """
+
     #   TESTING MASS TYPE LINE LOADS
 
     LoadCase(4, 'TYPE: Mass')
@@ -308,7 +308,7 @@ def test_line_set_loads():
     Line(2, '2 3')
     Line(3, '3 4')
     Line(4, '4 1')
-    
+
 
     Line(5, '5 6')
     Line(6, '6 7')
@@ -333,5 +333,5 @@ def test_line_set_loads():
     assert Model.clientModel.service.get_line_set_load(2, 1).magnitude == 2500
     assert Model.clientModel.service.get_line_set_load(3, 1).mass_global == 3100
     assert Model.clientModel.service.get_line_set_load(4, 1).magnitude == 4000
-    #assert Model.clientModel.service.get_line_set_load(5, 1).load_distribution == 'LOAD_DISTRIBUTION_CONCENTRATED_VARYING'
-    #assert Model.clientModel.service.get_line_set_load(6, 1).load_distribution == 'LOAD_DISTRIBUTION_VARYING'
+    assert Model.clientModel.service.get_line_set_load(5, 1).load_distribution == 'LOAD_DISTRIBUTION_CONCENTRATED_VARYING'
+    assert Model.clientModel.service.get_line_set_load(6, 1).load_distribution == 'LOAD_DISTRIBUTION_VARYING'
